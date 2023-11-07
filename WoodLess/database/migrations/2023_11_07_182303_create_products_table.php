8e53429141a3ec699d2ed9e4bca23a3493ff193e
Sgy157 <soggy706@googlemail.com>
2023-11-07 21:27:51 +0000

    Commented out images column - see comment

--- WoodLess/database/migrations/2023_11_07_182303_create_products_table.php
+++ WoodLess/database/migrations/2023_11_07_182303_create_products_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
              * Image Path. 
              * We should list the file path of images a product should use, so that we can retrieve them later.
              */
-            $table->string('images', 255);
+            //$table->string('images', 255);
 
             $table->json('attributes')->nullable();
             $table->json('tags')->nullable();
