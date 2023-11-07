bdf4c40fef2ca61d81d0dce5081cbacf561454b3
Sgy157 <soggy706@googlemail.com>
2023-11-07 16:56:11 +0000

    Added additional fields

--- WoodLess/database/migrations/2023_11_07_164108_create_cards_table.php
+++ WoodLess/database/migrations/2023_11_07_164108_create_cards_table.php
@@ -11,15 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name', 60);
-            $table->string('last_name', 60);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone_number', 15);
-            $table->boolean('admin')->default(false);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cards');
     }
 };
