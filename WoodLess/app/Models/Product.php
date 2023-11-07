6b3baf3341d2829e3ced425ee915780d54c9d904
Sgy157 <soggy706@googlemail.com>
2023-11-07 21:30:34 +0000

    Created

---
+++ WoodLess/app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//Used to populate DB with dummy data. Can be used to retrieve DB data.
+
+class Product extends Model
+{
+    use HasFactory;
+}
