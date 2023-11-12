8fe443946b57d398f1620e3bf20d38fe6036461a
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-11-12 20:32:48 +0000

    Links to pages
    Added links to other pages

--- WoodLess/resources/views/welcome.blade.php
+++ WoodLess/resources/views/welcome.blade.php
@@ -6,6 +6,23 @@
         <div class="row">
             <div class="col-12">
                 <h1>Welcome to WoodLess</h1>
+                <ul>
+                    <li>
+                        <a href="{{ url('about') }}">About Us</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('categories') }}">Categories</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('contact') }}">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('products-view') }}">Products Details</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('products') }}">Products List</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
