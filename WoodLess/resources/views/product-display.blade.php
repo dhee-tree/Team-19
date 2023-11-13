5f0fd75143275d2248a524c36e095197c900e477
Sgy157 <soggy706@googlemail.com>
2023-11-13 13:03:14 +0000

    Added database values

--- WoodLess/resources/views/product-display.blade.php
+++ WoodLess/resources/views/product-display.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.base')
-@section('title', 'WoodLess - Product Details')
+@section('title', 'WoodLess - '. $product->title)
+
+@php
+    $attributes = json_decode($product->attributes, true)
+@endphp
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Product Details</h1>
+                <h1>{{$product->title}}</h1>
+                <h2>About Product</h2>
+                <p>{{$product->description}}</p>
+                @foreach ($attributes as $attribute => $values)
+                    <p>{{'Attribute: '. $attribute . ' Value: '.$values}}</p>
+                @endforeach
             </div>
         </div>
     </div>
