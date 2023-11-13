f731b5f6265bb579ed5a31cadfaffb50a29d22b8
Sgy157 <soggy706@googlemail.com>
2023-11-13 13:02:27 +0000

    Updated product-view route, uses ProductController

--- WoodLess/routes/web.php
+++ WoodLess/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,7 @@ Route::view('/categories', 'categories');
 
 Route::view('/contact', 'contact');
 
-Route::view('/products-view', 'product-display');
+//Show single product
+Route::get('/product-view/{product}', [ProductController::class,'show']);
 
 Route::view('/products', 'product-list');
