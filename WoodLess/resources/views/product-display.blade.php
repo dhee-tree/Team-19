35b55d742be10bf920309a1f37a109fba222edf3
Sgy157 <soggy706@googlemail.com>
2023-11-13 13:39:20 +0000

    Added price

--- WoodLess/resources/views/product-display.blade.php
+++ WoodLess/resources/views/product-display.blade.php
@@ -6,16 +6,29 @@
 @endphp
 
 @section('content')
-    <div class="container">
+    <div class="container" id="product">
         <div class="row">
             <div class="col-12">
                 <h1>{{$product->title}}</h1>
+                <h3>£{{$product->cost}}</h3>
                 <h2>About Product</h2>
                 <p>{{$product->description}}</p>
                 @foreach ($attributes as $attribute => $values)
                     <p>{{'Attribute: '. $attribute . ' Value: '.$values}}</p>
                 @endforeach
             </div>
+        </div>
+    </div>
+
+    <div class="container" id="create-review">
+        <div class="col-12">
+            <h1>Add a Review</h1>
+        </div>
+    </div>
+
+    <div class="container" id="reviews">
+        <div class="col-12">
+            <h1>Reviews</h1>
         </div>
     </div>
 @endsection
