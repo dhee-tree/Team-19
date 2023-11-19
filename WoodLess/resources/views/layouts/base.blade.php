c08a4326339c7134dbfe8c6b60bc322d5b103a85
Umer Mohammed <D3m0nspider@gmail.com>
2023-11-19 16:40:09 +0000

    Added bootstrap version 5.3 to base. Added font awesome import to base. added poppins font to App.css.

--- WoodLess/resources/views/layouts/base.blade.php
+++ WoodLess/resources/views/layouts/base.blade.php
@@ -16,6 +16,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+        <!-- bootstrap 5.3 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         @yield('style')
 
@@ -36,5 +41,10 @@
                 <!-- Footer goes here -->
             </footer>
         </div>
+
+        <!-- bootstrap 5.3 -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
     </body>
 </html>
