87db32374edbcf132252bcaf7a60024c11f23cc1
Ismaeel Noor <220103820@aston.ac.uk>
2023-11-20 18:02:01 +0000

    about us test

--- WoodLess/resources/views/about.blade.php
+++ WoodLess/resources/views/about.blade.php
@@ -6,6 +6,40 @@
         <div class="row">
             <div class="col-12">
                 <h1>About Us</h1>
+                <a href="#conti">Jump to conti</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eligendi minima? Cum nostrum omnis at molestias facilis. Veniam perferendis ipsam asperiores minima necessitatibus laudantium perspiciatis. Necessitatibus aut perferendis dignissimos dolorem!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam rerum labore delectus neque, doloremque, cum hic rem tempora impedit repellendus excepturi autem quo tenetur commodi explicabo. Iure cupiditate rem deserunt.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, quas sint rem earum quia expedita aperiam quod eaque numquam nulla voluptates aut iste ea officia amet reprehenderit perspiciatis labore modi.
+                
+
+                <section id="conti">
+                    <h1>conti</h1>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quod voluptatem consequatur quaerat, aperiam vero fugiat commodi molestias a unde, cum architecto praesentium rem soluta laboriosam ab officia quas delectus.
+                </section>
             </div>
         </div>
     </div>
