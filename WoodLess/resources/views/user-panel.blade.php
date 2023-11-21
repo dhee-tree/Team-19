f22759c270df89f12b595f3c161b33fe39adeb0e
Zaakir04 <220140434@aston.ac.uk>
2023-11-21 15:40:38 +0000

    Using base layout

--- WoodLess/resources/views/user-panel.blade.php
+++ WoodLess/resources/views/user-panel.blade.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.base')
+@section('title', 'WoodLess - User Panel')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Products</h1>
+            </div>
+        </div>
+    </div>
+@endsection
