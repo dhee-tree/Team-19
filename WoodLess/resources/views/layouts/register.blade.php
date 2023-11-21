49c11ee88323fcd9a0d0b5cb20295eadd26f6076
Mustyjide19 <mustyjide19@gmail.com>
2023-11-21 15:05:04 +0100

    I added this

---
+++ WoodLess/resources/views/layouts/register.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+@section('title', 'WoodLess - Register page')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Registration</h1>
+            </div>
+        </div>
+    </div>
+@endsection
