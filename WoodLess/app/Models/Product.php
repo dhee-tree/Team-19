77e172eaff1cfc2a2ee92e11ecdf9d4818ffc8d5
Sgy157 <soggy706@googlemail.com>
2023-11-23 12:26:43 +0000

    Removed

--- WoodLess/app/Models/Product.php
+++ WoodLess/app/Models/Product.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-print(User::find('id'));
+
 class Product extends Model
 {
     use HasFactory;
