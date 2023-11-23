164d5341e0b2fce346935a23af8287709ca6060b
Sgy157 <soggy706@googlemail.com>
2023-11-23 11:57:33 +0000

    Added reviews field

--- WoodLess/app/Http/Controllers/ProductController.php
+++ WoodLess/app/Http/Controllers/ProductController.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //Single Product. Retrieves product from database using id and loads page
+    /**
+     * Retrives a Single Product.
+     */
     public function show(Product $product){
         return view('product-display', [
             'product' => $product,
             'attributes' => json_decode($product->attributes, true),
             'categories' => explode(',', $product->categories),
-            'productImages' => explode(',',$product->images)
+            'productImages' => explode(',',$product->images),
+            'reviews' => $product->reviews(),
         ]);
     }
 }
