5158db27b1de6a571e1b22db9e19768c3a7f7d5a
Umer Mohammed <D3m0nspider@gmail.com>
2023-11-26 22:23:35 +0000

    Edited style sheet Added better inbuilt padding using bootstraps systems.

--- WoodLess/resources/views/welcome.blade.php
+++ WoodLess/resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 @section('style')
 <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 <link rel="stylesheet" href="{{ asset('css/category-card.css') }}">
+<link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.2/components/testimonials/testimonial-3/assets/css/testimonial-3.css" />
+
 @endsection
 
 @section('content')
@@ -28,7 +30,7 @@
 
     </section>
 
-    <section id="Hot Sellers">
+    <section id="Hot Sellers" class="py-5 py-xl-8">
         <div class="container"> <!-- Carousel hot sellers -->
             <h1>
                 Hot Sellers
@@ -74,7 +76,7 @@
         </div>
     </section>
 
-    <section id="categories">
+    <section id="categories"  class="py-5 py-xl-8">
         <div class="container"> <!-- Carousel hot sellers -->
             <h1>
 
@@ -240,9 +242,70 @@
             </div>
     </section>
 
-    <section>
 
+    <!-- Testimonial 3 - Bootstrap Brain Component -->
+    <section id="testimonials" class="py-5 py-xl-8">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+                    <h2 class="fs-6 text-secondary mb-2 text-uppercase text-center">Happy Customers</h2>
+                    <p class="display-5 mb-4 mb-md-5 text-center">We deliver what we promise.</p>
+                    <hr class="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle">
+                </div>
+            </div>
+        </div>
+
+        <div class="container overflow-hidden">
+            <div class="row gy-4 gy-md-0 gx-xxl-5">
+                <div class="col-12 col-md-4">
+                    <div class="card border-0 border-bottom border-primary shadow-sm">
+                        <div class="card-body p-4 p-xxl-5">
+                            <figure>
+                                <img class="img-fluid rounded rounded-circle mb-4 border border-5" loading="lazy" src="./assets/img/testimonial-img-1.jpg" alt="">
+                                <figcaption>
+                                    <div class="bsb-ratings text-warning mb-3" data-bsb-star="5" data-bsb-star-off="0"></div>
+                                    <blockquote class="bsb-blockquote-icon mb-4">Nam ultricies, ex lacinia dapibus faucibus, sapien ipsum euismod massa, at aliquet erat turpis quis diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</blockquote>
+                                    <h4 class="mb-2">Luna John</h4>
+                                    <h5 class="fs-6 text-secondary mb-0">UX Designer</h5>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="card border-0 border-bottom border-primary shadow-sm">
+                        <div class="card-body p-4 p-xxl-5">
+                            <figure>
+                                <img class="img-fluid rounded rounded-circle mb-4 border border-5" loading="lazy" src="./assets/img/testimonial-img-2.jpg" alt="">
+                                <figcaption>
+                                    <div class="bsb-ratings text-warning mb-3" data-bsb-star="4" data-bsb-star-off="1"></div>
+                                    <blockquote class="bsb-blockquote-icon mb-4">Nam ultricies, ex lacinia dapibus faucibus, sapien ipsum euismod massa, at aliquet erat turpis quis diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</blockquote>
+                                    <h4 class="mb-2">Mark Smith</h4>
+                                    <h5 class="fs-6 text-secondary mb-0">Marketing Specialist</h5>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="card border-0 border-bottom border-primary shadow-sm">
+                        <div class="card-body p-4 p-xxl-5">
+                            <figure>
+                                <img class="img-fluid rounded rounded-circle mb-4 border border-5" loading="lazy" src="./assets/img/testimonial-img-4.jpg" alt="">
+                                <figcaption>
+                                    <div class="bsb-ratings text-warning mb-3" data-bsb-star="5" data-bsb-star-off="0"></div>
+                                    <blockquote class="bsb-blockquote-icon mb-4">Nam ultricies, ex lacinia dapibus faucibus, sapien ipsum euismod massa, at aliquet erat turpis quis diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</blockquote>
+                                    <h4 class="mb-2">Luke Reeves</h4>
+                                    <h5 class="fs-6 text-secondary mb-0">Sales Manager</h5>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
+
 </div>
 
 @endsection
