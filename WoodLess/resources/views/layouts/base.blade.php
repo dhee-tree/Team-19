dd233f00343c6d6f756eee0910e72afff3cfbbd9
220109796 <220109796@aston.ac.uk>
2023-11-26 21:31:20 +0000

    Completed footer (except size issue)

--- WoodLess/resources/views/layouts/base.blade.php
+++ WoodLess/resources/views/layouts/base.blade.php
@@ -22,8 +22,8 @@
     <!-- bootstrap 5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-        <!-- Font Awesome JS -->
-        <script src="https://kit.fontawesome.com/c5cd4f3e40.js" crossorigin="anonymous"></script>
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/c5cd4f3e40.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @yield('style')
@@ -71,58 +71,55 @@
             @yield('content')
         </main>
 
-        <footer>
-            <div class="container">
-                <footer class="row row-cols-5 py-5 my-5 border-top">
-                    <div class="col">
-                        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-                            <svg class="bi me-2" width="40" height="32">
-                                <use xlink:href="#Bootstrap"></use>
-                            </svg>
-                        </a>
-                        <p class="text-muted">© 2023</p>
-                    </div>
-
-                    <div class="col">
-
-                    </div>
-
-                    <div class="col">
-                        <h5>Site Map</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="/Team-19/WoodLess/public/" class="nav-link p-0 text-muted">Home</a></li>
-                            <li class="nav-item mb-2"><a href="products" class="nav-link p-0 text-muted">Products</a></li>
-                            <li class="nav-item mb-2"><a href="basket" class="nav-link p-0 text-muted">Basket</a></li>
-                            <li class="nav-item mb-2"><a href="team" class="nav-link p-0 text-muted">Meet the Team</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col">
-                        <h5>‎</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="contact" class="nav-link p-0 text-muted">Contact Us</a></li>
-                            <li class="nav-item mb-2"><a href="about" class="nav-link p-0 text-muted">About Us</a></li>
-                            <li class="nav-item mb-2"><a href="register" class="nav-link p-0 text-muted">Register</a></li>
-                            <li class="nav-item mb-2"><a href="login" class="nav-link p-0 text-muted">Login</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col">
-                        <h5>Our Socials</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-instagram fa-xl"></i></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-x-twitter fa-xl"></i></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-square-facebook fa-xl"></i></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-linkedin fa-xl"></i></a></li>
-                        </ul>
-                    </div>
-                </footer>
+        <footer class="row row-cols-5 py-5 bg-dark">
+            <div class="col">
+                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32">
+                        <use xlink:href="#Bootstrap"></use>
+                    </svg>
+                </a>
+                <p class="ps-4 pt-2" style="color: #a9a9a9;">© 2023</p>
             </div>
 
-            <!-- bootstrap 5.3 -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            <div class="col">
 
-            @yield('js')
+            </div>
+
+            <div class="col">
+                <h5 class="fw-bold">Site Map</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="{{ url('/') }}" class="nav-link p-0" style="color: #ffffff;">Home</a></li>
+                    <li class="nav-item mb-2"><a href="products" class="nav-link p-0" style="color: #ffffff;">Products</a></li>
+                    <li class="nav-item mb-2"><a href="basket" class="nav-link p-0" style="color: #ffffff;">Basket</a></li>
+                    <li class="nav-item mb-2"><a href="team" class="nav-link p-0" style="color: #ffffff;">Meet the Team</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h5>‎</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="contact" class="nav-link p-0" style="color: #ffffff;">Contact Us</a></li>
+                    <li class="nav-item mb-2"><a href="about" class="nav-link p-0" style="color: #ffffff;">About Us</a></li>
+                    <li class="nav-item mb-2"><a href="register" class="nav-link p-0" style="color: #ffffff;">Register</a></li>
+                    <li class="nav-item mb-2"><a href="login" class="nav-link p-0" style="color: #ffffff;">Login</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h5 class="fw-bold">Our Socials</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #ffffff;"><i class="fa-brands fa-instagram fa-xl"></i></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #ffffff;"><i class="fa-brands fa-x-twitter fa-xl"></i></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #ffffff;"><i class="fa-brands fa-square-facebook fa-xl"></i></a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #ffffff;"><i class="fa-brands fa-linkedin fa-xl"></i></a></li>
+                </ul>
+            </div>
+        </footer>
+
+        <!-- bootstrap 5.3 -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+        @yield('js')
 </body>
 
 </html>
