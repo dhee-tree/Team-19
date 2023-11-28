ecbcb0908c42fbf8f753d7cf61a567941e493c9c
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-11-28 10:48:43 +0000

    Added form action, method, csrf and names to fields

--- WoodLess/resources/views/contact.blade.php
+++ WoodLess/resources/views/contact.blade.php
@@ -29,18 +29,35 @@
 
             <div class="col-md-6">
                 <div class="contact-card">
-                    <form>
+                    @if (session()->has('success'))
+                        <div class="text-sm font-medium ml-3">Success!.</div>
+                        <div class="text-sm tracking-wide text-gray-500 dark:text-white mt-4 sm:mt-0 sm:ml-4"> {{ session('success') }}</div>
+                    @endif
+                    <form action="{{ route('contact.send') }}" method="post">
+                        @csrf
                         <label for="name" class="form-label">Name: </label>
-                        <input type="text" class="form-control" id="name" placeholder="John Doe">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="John Doe">
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
 
                         <label for="email" class="form-label">Email: </label>
-                        <input type="email" class="form-control" id="email" placeholder="john.doe@example.com">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="john.doe@example.com">
+                        @error('email')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
 
                         <label for="phone" class="form-label">Phone: </label>
-                        <input type="tel" class="form-control" id="phone" placeholder="+44 0000 000000">
+                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="+44 0000 000000">
+                        @error('phone')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
 
                         <label for="message" class="form-label">Message: </label>
-                        <textarea class="form-control" id="message" rows="7" placeholder="Your message here..."></textarea>
+                        <textarea class="form-control" id="message" name="message" rows="7" placeholder="Your message here..."></textarea>
+                        @error('message')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
 
                         <button type="submit" class="btn contact-btn">Submit</button>
                     </form>
