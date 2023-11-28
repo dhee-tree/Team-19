a83b9316a5815b6fb437b81183d772edc8e2a26c
Zaakir04 <220140434@aston.ac.uk>
2023-11-28 14:37:37 +0000

    Updated categories page and stlying.

--- WoodLess/resources/views/categories.blade.php
+++ WoodLess/resources/views/categories.blade.php
@@ -1,13 +1,183 @@
 @extends('layouts.base')
 @section('title', 'WoodLess - Categories')
+@section('style')
+
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/category-card.css') }}">
+@endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Categories Page</h1>
-                I edited  this page.
+   
+
+<section id="categories">
+        <div class="container"> <!-- Carousel hot sellers -->
+            <h1>
+
+                Categories
+
+            </h1>
+            <!-- TODO ADD CODE THAT SENDS TO THE FILTER PAGE WITH THE CATEGORY PICKED FILTER. -->
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Category image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Category details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Category name-->
+                                    <h5 class="fw-bolder">Kitchen Category</h5>
+                                    <!-- Category price-->
+                                    Stuff Here
+                                </div>
+                            </div>
+                            <!-- Category actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+    </section>
+
+    <section>
+
+    </section>
+</div>
+
 @endsection
