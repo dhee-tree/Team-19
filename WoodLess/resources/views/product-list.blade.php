b217475d8bbe914662b5269a7e17eaa0a1518c26
Umer Mohammed <D3m0nspider@gmail.com>
2023-11-28 14:21:59 +0000

    Styled item filter added filter

--- WoodLess/resources/views/product-list.blade.php
+++ WoodLess/resources/views/product-list.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.base')
 @section('title', 'WoodLess - Products')
+@section('style')
+<link rel="stylesheet" href="{{ asset('css/item-filter.css') }}">
+@endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+
+            <?php include(app_path().'/includes/item-filter.php'); ?>
+        </div>
+        <div class="col-md-9">
+            <div class="container-fluid">
                 <h1>Products</h1>
+                <div class="row">
+                    <div class="col min-vh-100 py-3">
+                        <!-- toggler -->
+                        <button class="btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
+                            <i class="fa-solid fa-arrow-right fs-3" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvas"></i>
+                        </button>
+                        Content..
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
