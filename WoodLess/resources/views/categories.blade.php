e3584071b5ee18cd0f304a94203e98d9b252cde5
Zaakir04 <220140434@aston.ac.uk>
2023-11-28 16:11:52 +0000

    Completed the ctegories page

--- WoodLess/resources/views/categories.blade.php
+++ WoodLess/resources/views/categories.blade.php
@@ -4,180 +4,125 @@
 
 <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 <link rel="stylesheet" href="{{ asset('css/category-card.css') }}">
+<link rel="stylesheet" href="{{ asset('css/categories.css') }}">
 @endsection
 
 @section('content')
    
 
-<section id="categories">
+<section id="categories" class="py-5 py-xl-8">
+    
+        <div class="container-fluid">
+            <div class="row justify-content-md-center">
+                <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+                    <h2 id= "title" class="fs-6 text-secondary mb-2 text-uppercase text-center">Welcome To Our Categories</h2>
+                    <hr class="w-50 mx-auto mb-4 mb-xl-5 border-dark">
+                </div>
+            </div>
+        </div>
         <div class="container"> <!-- Carousel hot sellers -->
-            <h1>
-
-                Categories
-
-            </h1>
             <!-- TODO ADD CODE THAT SENDS TO THE FILTER PAGE WITH THE CATEGORY PICKED FILTER. -->
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div class="container px-4 px-lg-4 mt-2">
+                <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-3 justify-content-center">
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
                     </div>
                     <div class="col mb-5">
-                        <div class="card h-100">
+                        <div class="card category-card h-100">
                             <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img"
+                                src="https://i0.wp.com/kitchenmakeovers.co.uk/wp-content/uploads/2022/03/Jayline-Supermatt-Indigo-Blue-scaled-1.jpg"
+                                height="100%" width="100%" alt="..." />
                             <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
+                            <div class="card-img-overlay d-flex flex-column align-items-center">
+
+                                <!-- Category actions-->
+
+                                <a class="btn mt-auto stretched-link shadow border border-info" href="#">Kitchen
+                                    Category</a>
                             </div>
                         </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Category image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Category details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Category name-->
-                                    <h5 class="fw-bolder">Kitchen Category</h5>
-                                    <!-- Category price-->
-                                    Stuff Here
-                                </div>
-                            </div>
-                            <!-- Category actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View Products</a></div>
-                            </div>
-                        </div>
-                    </div>
+                    </div>  
                 </div>
             </div>
     </section>
 
-    <section>
-
-    </section>
-</div>
 
 @endsection
