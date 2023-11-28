02ed4c2106fa30b5dc160287eed6c18a40b553ed
Sgy157 <soggy706@googlemail.com>
2023-11-28 18:22:31 +0000

    Removed items row, User id is now PK

--- WoodLess/database/migrations/2023_11_07_175217_create_baskets_table.php
+++ WoodLess/database/migrations/2023_11_07_175217_create_baskets_table.php
@@ -12,15 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('baskets', function (Blueprint $table) {
-            $table->id();
-            $table->json('items')->nullable();
-            $table->integer('total_cost');
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->primary('user_id');
+            $table->integer('total_cost')->default(0);
             $table->timestamps();
         });
 
         // Add foregin key constraint
         Schema::table('baskets', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
