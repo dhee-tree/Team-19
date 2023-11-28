a9c2b5296cf56e084fa8e822dc399bb10f2de645
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-11-28 10:52:29 +0000

    Added functions contact get, contact post methods with mail to admin

--- WoodLess/app/Http/Controllers/ContactController.php
+++ WoodLess/app/Http/Controllers/ContactController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactUsMail;
 
 class ContactController extends Controller
 {
-    //
+    public function showForm()
+    {
+        return view('contact');
+    }
+
+    public function sendEmail()
+    {
+        $data = request()->validate([
+            'name' => 'required|min:2|max:255',
+            'email' => 'required|email',
+            'phone' => 'required|numeric',
+            'message' => 'required|min:5|max:1000',
+        ]);
+        Mail::to(env('MAIL_FROM_ADDRESS'))->send(new ContactUsMail($data));
+        return redirect()->back()->with('success', 'Your message has been sent successfully!');
+    }
 }
