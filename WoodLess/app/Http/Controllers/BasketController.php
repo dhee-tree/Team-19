41f2b008a4213ff1a3834a440b105b78b78421d8
Sgy157 <soggy706@googlemail.com>
2023-11-29 22:24:38 +0000

    Inital implement Item stores into basket with id 1

--- WoodLess/app/Http/Controllers/BasketController.php
+++ WoodLess/app/Http/Controllers/BasketController.php
@@ -3,26 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class BasketController extends Controller
 {
     /**
-     * Store item from basket.
+     * Store Product in a basket.
      */
     function store(Request $request, Product $product){
 
-        $formFields = $request->validate([
-            'attribute-color' => 'required',
+        //$user = auth()->user()->basket;
+
+        //Currently stores in first basket in db.
+        $basket = User::where("id", 1)->first()->basket;
+
+        $attributes = [];
+
+        $rules = [
+            'attributes' => 'required',
+            'amount' => 'required',
+        ];
+
+        if($request->has('attribute-colours')){
+            $rules['attribute-colours'] = 'required|not_in:0';
+            $request->validate($rules);
+
+            $pairs = explode(':', $request['attribute-colours']);
+            $attributes[$pairs[0]] = $pairs[1];
+        };
+
+        $request->validate($rules);
+
+        foreach($request['attributes'] as $attribute){
+            $pairs = explode(':', $attribute);
+            $attributes[$pairs[0]] = $pairs[1];
+        };
+
+        $basket->products()->attach($product->id,[
+            'amount' => $request['amount'],
+            'attributes' => json_encode($attributes),
         ]);
 
-        //Attach(product id, [amount of items, attributes])
-
-        /*$user->basket->products()->attach(1,[
-            'amount' => 10,
-            //'attributes' => //JSON Here
-        ]);*/
-
-        return back()->with('message', 'Item added to basket.');
+        return back()->with('message', 'Item(s) added to basket.');
     }
 }
