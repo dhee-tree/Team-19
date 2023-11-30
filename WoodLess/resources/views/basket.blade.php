d641745a6cfad038aeb491deac126a3ace60ad28
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-11-30 00:47:37 +0000

    Added basket display blade

---
+++ WoodLess/resources/views/basket.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.base')
+@section('title', 'WoodLess - Contact Us')
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/basket.css') }}">
+@endsection 
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Your Basket</h1>
+            </div>  
+        </div>  
+    </div>  
+@endsection
+
+@section('js')
+<!-- <script  rel= "jssheet" src="{{ asset('js/basket.js') }}"></script> -->
+@endsection
