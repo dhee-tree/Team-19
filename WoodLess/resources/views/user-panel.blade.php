2d86c128d284c541f02e2082279e443c93b75822
Zaakir04 <220140434@aston.ac.uk>
2023-12-01 18:38:31 +0000

    Added user panel page links for different sections

--- WoodLess/resources/views/user-panel.blade.php
+++ WoodLess/resources/views/user-panel.blade.php
@@ -2,11 +2,52 @@
 @section('title', 'WoodLess - User Panel')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>User-panel</h1>
+<body>
+    <!-- Code for sidebar -->
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="sidebar">
+                <div class="heading">
+                    <div>Hi, Welcome back to your account.
+                    </div>
+                <ul class="sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            My Purchases
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            My Points
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            Contact Us
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            Sign Out
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-regular fa-clipboard-list pe-2"></i>
+                            Invite A Friend
+                        </a>
+                    </li>
+                </ul>
             </div>
-        </div>
-    </div>
+        </aside>
 @endsection
