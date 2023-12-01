dd5d43ccecf204d539efd5f393765af10211df2b
Sgy157 <soggy706@googlemail.com>
2023-12-01 12:34:24 +0000

    Closable alert

--- WoodLess/resources/views/layouts/alert.blade.php
+++ WoodLess/resources/views/layouts/alert.blade.php
@@ -1,6 +1,6 @@
 @if(session('status'))
         
-<div class="mt-4 alert alert-{{session('status')}}" role="alert">
+<div class="mt-4 alert alert-{{session('status')}} alert-dismissible fade show" role="alert">
     @if(session('status') == 'success')
         <i class="fa-solid fa-xs fa-check"></i>
     @else
@@ -8,6 +8,7 @@
     @endif
 
     {{session('message')}}
+    <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 
 <hr class="mt-0">
