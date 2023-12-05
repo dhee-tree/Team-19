0d4f884dad499247524c7315eb4590540aabf4cd
Ismaeel Noor <220103820@aston.ac.uk>
2023-12-05 15:43:30 +0000

    products displayed

--- WoodLess/resources/views/product-list.blade.php
+++ WoodLess/resources/views/product-list.blade.php
@@ -6,6 +6,9 @@
         <div class="row">
             <div class="col-12">
                 <h1>Products</h1>
+                @foreach ($products as $product)
+    <p>{{ $product->title }}-{{ $product->description }}-{{ $product->cost }} </p>
+@endforeach
             </div>
         </div>
     </div>
