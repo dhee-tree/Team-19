599296a8bb22df825454f70e244db255bf49648b
Umer Mohammed <D3m0nspider@gmail.com>
2023-12-05 15:39:02 +0000

    Added product display card with sale addon

--- WoodLess/resources/views/product-list.blade.php
+++ WoodLess/resources/views/product-list.blade.php
@@ -23,19 +23,40 @@
         </div>
         <div class="col-md-9">
             <div class="container-fluid">
-                <h1>Products</h1>
+
                 <div class="row">
-                    <div class="colpy-3">
-                        <!-- toggler -->
-                        <button class="btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
-                            <i class="fa-solid fa-arrow-right fs-3" data-bs-toggle="offcanvas"
-                                data-bs-target="#offcanvas"></i>
-                        </button>
-                        Content..
+                    <div class="container px-4 px-lg-5 mt-5">
+                        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                            <div class="col mb-5">
+                                <div class="card h-100">
+                                    <!-- Sale badge-->
+                                    <div class="badge bg-dark text-white position-absolute"
+                                        style="top: 0.5rem; right: 0.5rem">Sale</div>
+                                    <!-- Product image-->
+                                    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                        alt="..." />
+                                    <!-- Product details-->
+                                    <div class="card-body p-4">
+                                        <div class="text-center">
+                                            <!-- Product name-->
+                                            <h5 class="fw-bolder">Fancy Product</h5>
+                                            <!-- Product price-->
+                                            $40.00 - $80.00
+                                        </div>
+                                    </div>
+                                    <!-- Product actions-->
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View
+                                                options</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
