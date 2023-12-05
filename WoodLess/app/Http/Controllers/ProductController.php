ee34acd37166512d33569b235b173a8b54cc3b68
Sgy157 <soggy706@googlemail.com>
2023-12-05 15:48:28 +0000

    Inital similar product implementation

--- WoodLess/app/Http/Controllers/ProductController.php
+++ WoodLess/app/Http/Controllers/ProductController.php
@@ -13,18 +13,16 @@ class ProductController extends Controller
     /**
      * Retrieve a single product.
      */
-    public function show(Request $request, Product $product){
+    public function show(Product $product){
 
         $product->loadMissing('categories', 'reviews');
+
+        $similarProducts = Product::all()->where('id', 6)->take(6);
         
         $reviews = $product->reviews()->orderBy(
             request('sort') ?? 'created_at',
             request('order') ?? 'desc'
         )->paginate(5)->withQueryString()->fragment('reviews');
-
-        if ($request->ajax()) {
-            return view('reviews.load', ['reviews' => $reviews])->render();  
-        }
 
         return view('product-display', [
             'product' => $product,
@@ -32,6 +30,7 @@ class ProductController extends Controller
             'categories' => $product->categories()->get(),
             'productImages' => explode(',', $product->images),
             'reviews' => $reviews,
+            'similarProducts' => $similarProducts,
             'finalCost' => sprintf("%0.2f",round(($product->cost)-(($product->cost) * ($product->discount/100)),2)),
         ])->render();
     }
