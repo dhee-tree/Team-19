e2c3063614be7dcfeb2ada7107c581ef77e98c11
Umer Mohammed <D3m0nspider@gmail.com>
2023-12-05 14:57:11 +0000

    Edited the title for product page edited styling, removing margin

--- WoodLess/resources/views/product-list.blade.php
+++ WoodLess/resources/views/product-list.blade.php
@@ -12,7 +12,6 @@
         <div class="row justify-content-md-center">
             <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
                 <h2 class="fs-6 text-secondary mb-2 text-uppercase text-center">Products</h2>
-                <p class="display-5 mb-2 mb-md-3 text-center">Please take your time and enjoy!</p>
                 <hr class="w-50 mx-auto mb-4 mb-xl-5 border-dark">
             </div>
         </div>
