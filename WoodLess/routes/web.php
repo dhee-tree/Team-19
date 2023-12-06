248e019d4b82d6a8e64fb0c4b1b6a9e99be09de0
NduNdu21 <ndumisobm@icloud.com>
2023-12-06 16:02:12 +0000

    added routes to admin-panel subpages

--- WoodLess/routes/web.php
+++ WoodLess/routes/web.php
@@ -51,3 +51,9 @@ Route::view('/admin-panel', 'admin-panel');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//admin panel pages
+Route::view('/admin-panel/orders', 'orders-admin');
+Route::view('/admin-panel/tickets', 'tickets-admin');
+Route::view('/admin-panel/users', 'users-admin');
+Route::view('/admin-panel/inventory', 'inventory');
