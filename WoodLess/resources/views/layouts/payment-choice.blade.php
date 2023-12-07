d4a89adb75cdf1f2ffb8e043d3a7ec0118e82a67
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-12-07 12:18:12 +0000

    Basic payment choice template

---
+++ WoodLess/resources/views/layouts/payment-choice.blade.php
@@ -0,0 +1,7 @@
+<div id="choice-paypal">
+    <h4>Your are paying with PayPal</h4>
+    <div id="paypal-button-container"></div>
+
+    <h5>Enter your email: </h5>
+    <input type="email" name="" id="">
+</div>
