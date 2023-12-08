c0f4a6ff2e62f0eefe330f72c7048fd76878d582
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-12-08 01:28:45 +0000

    Checkout updates total cost of the basket on database

--- WoodLess/app/Http/Controllers/CheckoutController.php
+++ WoodLess/app/Http/Controllers/CheckoutController.php
@@ -14,7 +14,12 @@ class CheckoutController extends Controller
 {
     function show(){
         $basket = auth()->user()->basket;
-        // $basket->loadMissing('products');
+        // Update total cost of basket on the database before loading it.
+        $basket->total_cost = $basket->totalCost();
+        $basket->save();
+        
+        $basket->loadMissing('products');
+        
         return view('checkout', [
             'basket' => $basket,
             // 'address' => auth()->user()->address,
