a587ab5aba3e7455f2afa2f07ea6fbf4ea594dca
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-12-08 02:28:25 +0000

    Added order confirmation template and email markdown

---
+++ WoodLess/resources/views/order-confirmation.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+@section('title', 'WoodLess - Order Confirmed')
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/checkout.css') }}">
+@endsection 
+
+@section('content')
+<div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4 mt-5">
+        <h2>Order Confirmed</h2>
+        <p>Thank you for your order. Your order number is: {{ $basket->id }}</p>
+        <p>You will receive an email confirmation shortly.</p>
+        <p>Click <a href="{{ route('home') }}">here</a> to return to the home page.</p>
+    </div>
+    <div class="col-md-4"></div>
+</div>
+@endsection
