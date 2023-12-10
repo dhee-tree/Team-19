24e34ca32be9d8474f8bdd3c5f47667ba4c0c2c9
Ismaeel Noor <220103820@aston.ac.uk>
2023-12-10 17:34:38 +0000

    fixed a bug

--- WoodLess/routes/web.php
+++ WoodLess/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BasketController;
@@ -9,6 +9,10 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserPanelController;
 use App\Http\Controllers\ChangePasswordController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\CategoryController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -36,29 +40,35 @@ Route::post('/contact', [ContactController::class, 'sendEmail'])->name('contact.
 Route::get('/product/{product}', [ProductController::class,'show']);
 
 // Basket URLS
-Route::get('/basket', [BasketController::class,'show']);
+Route::get('/basket', [BasketController::class,'show'])->middleware('auth');
 //Store product in basket
-Route::post('/basket/{product}', [BasketController::class,'store']);
+Route::post('/basket/{product}', [BasketController::class,'store'])->middleware('auth');
 // Delete product from basket
 Route::delete('/basket/{basket}', [BasketController::class,'destroy'])->name('basket.destroy');
 
+// Checkout URLS
+Route::get('/checkout', [CheckoutController::class,'show']);
+Route::post('/checkout', [CheckoutController::class,'store'])->name('checkout.store')->middleware('auth');
+
 //Store single review
-Route::post('/review/{product}', [ReviewController::class,'store']);
+Route::post('/review/{product}', [ReviewController::class,'store'])->middleware('auth');
 //Delete single review
 Route::delete('/review/{review}', [ReviewController::class,'destroy']);
 
+//showcases products
 Route::get('/products', [ProductController::class,'index']);
 
 Route::view('/admin-panel', 'admin-panel');
 
-Auth::routes();
-Route::get('/categories', [CategoryController::class, 'getCategories']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Auth::routes();
+// Display categories
 
+Auth::routes();
+Route::get('/categories', [CategoryController::class, 'getCategories']);
 // User panel links
 Route::get('/user-panel', [App\Http\Controllers\UserPanelController::class, 'show'])->name('user-panel')->middleware('auth');
 
