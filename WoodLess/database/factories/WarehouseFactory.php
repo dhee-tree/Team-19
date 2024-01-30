7b1e6aa40dabbf28925b7f4744078a92fe8182d4
Sgy157 <soggy706@googlemail.com>
2024-01-30 13:29:26 +0000

    Added

---
+++ WoodLess/database/factories/WarehouseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Warehouse>
+ */
+class WarehouseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'address' => fake()->streetAddress(),
+            'address_2' => fake()->streetAddress(),
+            'postcode' => fake()->postcode(),
+            'city' => fake()->city(),
+        ];
+    }
+}
