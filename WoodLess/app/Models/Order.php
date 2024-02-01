1d212ff30040daa446cfb91ee6f8ccfaf2b87b41
Sgy157 <soggy706@googlemail.com>
2024-02-01 14:27:03 +0000

    Added, relationship functions

---
+++ WoodLess/app/Models/Order.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'warehouse_id',
+        'address_id',
+    ];
+
+    /**
+     * Returns the user associated with the order.
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Returns the products associated with the order and relevant order information.
+     */
+    public function products(){
+        return $this->belongsToMany(Product::class, 'order_product_warehouse')->withPivot('amount','warehouse_id','attributes');
+    }
+
+    
+}
