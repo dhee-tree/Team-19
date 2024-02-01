5ea24238b148de15a183a3a9f77ad71f623d181c
NduNdu21 <ndumisobm@icloud.com>
2024-02-01 14:16:37 +0000

    added content + changed links to panel pages

--- WoodLess/resources/views/admin-panel.blade.php
+++ WoodLess/resources/views/admin-panel.blade.php
@@ -8,48 +8,136 @@
 
 @section('content')
 <body>
-    <div class="container">
-        <!-- sidebar -->
-        <div class="sidebar bg-dark">
-            <a href="#" class="logo">
-                <div class="logo-name"><span>Admin</span>Panel</div>
+    <!-- sidebar -->
+    <div class="sidebar">
+        <a href="#" class="logo">
+            <div class="logo-name"><span>Admin</span>Panel</div>
+        </a>
+        <ul class="side-menu">
+            <li><a href="#"><i class="fa-solid fa-house"></i></i>Dashboard</a></li>
+            <li><a href="{{url('/admin-panel/inventory')}}"><i class="fa-solid fa-warehouse"></i></i>Inventory</a></li>
+            <li class="active"><a href="{{url('/admin-panel/orders')}}"><i class="fa-solid fa-truck-moving"></i>Orders</a></li>
+            <li><a href="{{url('/admin-panel/tickets')}}"><i class="fa-solid fa-message"></i>Tickets</a></li>
+            <li><a href="{{url('/admin-panel/users')}}"><i class="fa-solid fa-user"></i>Users</a></li>
+            <li><a href="#"><i class="fa-solid fa-gear"></i>Settings</a></li>
+        </ul>
+        <ul class="side-menu">
+            <li>
+                <a href="#" class="logout">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- end of sidebar -->
+
+    <!-- main content -->
+    <div class="content">
+        <nav>
+            <i class="fa-solid fa-bars"></i>
+            <form action="#">
+                <div class="form-input">
+                    <input type="search" placeholder="Search...">
+                    <button class="search-btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
+            <a href="#" class="notif">
+                <i class="fa-solid fa-bell"></i>
+                <span class="count">12</span>
             </a>
-            <ul class="side-menu">
-                <li><a href="#"><i class="fa-solid fa-house"></i></i>Dashboard</a></li>
-                <li><a href="#"><i class="fa-solid fa-warehouse"></i></i>Shop</a></li>
-                <li class="active"><a href="#"><i class="fa-solid fa-truck-moving"></i>Orders</a></li>
-                <li><a href="#"><i class="fa-solid fa-message"></i>Tickets</a></li>
-                <li><a href="#"><i class="fa-solid fa-user"></i>Users</a></li>
-                <li><a href="#"><i class="fa-solid fa-gear"></i>Settings</a></li>
-            </ul>
-            <ul class="side-menu">
+        </nav>
+
+        <main>
+            <div class="header">
+                <div class="left">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+
+            <!-- Insights -->
+            <ul class="insights">
                 <li>
-                    <a href="#" class="logout">
-                        <i class="fa-solid fa-right-from-bracket"></i>
-                        Logout
-                    </a>
+                    <i class="fa-solid fa-clipboard-check"></i>
+                    <span class="info">
+                        <h3>
+                            1,074
+                        </h3>
+                        <p>Paid Orders</p>
+                    </span>
+                </li>
+                <li><i class="fa-solid fa-user"></i>
+                    <span class="info">
+                        <h3>
+                            3,944
+                        </h3>
+                        <p>Users</p>
+                    </span>
+                </li>
+                <li><i class="fa-solid fa-comments"></i>
+                    <span class="info">
+                        <h3>
+                            14,721
+                        </h3>
+                        <p>Tickets</p>
+                    </span>
+                </li>
+                <li><i class="fa-solid fa-money-bill-wave"></i>
+                    <span class="info">
+                        <h3>
+                            £6,742
+                        </h3>
+                        <p>Total Sales</p>
+                    </span>
                 </li>
             </ul>
-        </div>
-        <!-- end of sidebar -->
+            <!-- End of Insights -->
 
-        <!-- main content -->
-        <div class="content">
-            <nav>
-                <i class="fa-solid fa-bars"></i>
-                <form action="#">
-                    <div class="form-input">
-                        <input type="search" placeholder="Search...">
-                        <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
+            <!-- Recent orders table -->
+            <div class="bottom-data">
+                <div class="orders">
+                    <div class="header">
+                        <i class='bx bx-receipt'></i>
+                        <h3>Recent Orders</h3>
+                        <i class='bx bx-filter'></i>
+                        <i class='bx bx-search'></i>
                     </div>
-                </form>
-                <a href="#" class="notif">
-                    <i class="fa-solid fa-bell"></i>
-                    <span class="count">12</span>
-                </a>
-            </nav>
-        </div>
-        <!-- end of content -->
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>Order Date</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p>John Doe</p>
+                                </td>
+                                <td>14-08-2023</td>
+                                <td><span class="status completed">Completed</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>John Doe</p>
+                                </td>
+                                <td>14-08-2023</td>
+                                <td><span class="status pending">Pending</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>John Doe</p>
+                                </td>
+                                <td>14-08-2023</td>
+                                <td><span class="status process">Processing</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
     </div>
+    <!-- end of content -->
 </body>
 @endsection
