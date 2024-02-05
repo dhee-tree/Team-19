060944c040685dae8c4b953a04d75d5c7e6da4fc
Mustyjide19 <mustyjide19@gmail.com>
2024-02-05 03:06:24 +0100

    Made changes to register controller and added email verification

--- WoodLess/app/Models/User.php
+++ WoodLess/app/Models/User.php
@@ -2,72 +2,109 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
         'phone_number',
         'email',
         'password',
+        'verification_code',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
         'is_admin',
     ];
 
-    protected $guarded =[
-        'is_admin'
+    protected $guarded = [
+        'is_admin',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
-    /**
-     * Get the reviews associated with the user.
-     */
     public function reviews()
     {
         return $this->hasMany(Review::class);
     }
 
-    /**
-     * Get the basket associated with the user.
-     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+
     public function basket()
     {
         return $this->hasOne(Basket::class);
     }
 
-    
-    public function isAdmin(){
+    public function isAdmin()
+    {
         return $this->makeVisible('is_admin')->is_admin;
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new \Illuminate\Auth\Notifications\VerifyEmail);
+    }
+
+    public function markEmailAsVerified()
+    {
+        return $this->forceFill([
+            'email_verified_at' => $this->freshTimestamp(),
+            'verification_code' => null,
+        ])->save();
+    }
+
+    public function hasVerifiedEmail()
+    {
+        return !is_null($this->email_verified_at);
+    }
+
+    public function getEmailForVerification()
+    {
+        return $this->email;
+    }
+
+    public function createEmailVerificationCode()
+    {
+        $this->verification_code = \Illuminate\Support\Str::random(40);
+        $this->save();
+    }
+
+    public function verificationCodeIsValid($code)
+    {
+        return $this->verification_code === $code;
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            if (!Schema::hasColumn('users', 'verification_code')) {
+                $user->createEmailVerificationCode();
+            }
+        });
     }
 }
