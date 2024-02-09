57f41d5dcc5c1887421fbdca07342c6d1efa22b5
Mustyjide19 <mustyjide19@gmail.com>
2024-02-09 13:58:55 +0100

    created new functions for the verification code with timestamps

--- WoodLess/app/Models/User.php
+++ WoodLess/app/Models/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -70,13 +69,8 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     public function emailVerificationCodes(){
-        return $this->hasMany(EmailVerificationCode::class);
+        return $this->belongsToMany(EmailVerificationCode::class, 'email_verification_codes', 'user_id','code')->withTimeStamps();
     }
-
-    public function generateEmailVerificationCode(int $code){
-        $this->emailVerificationCodes()->attach(['code' => $code]);
-    }
-
     
     public function isAdmin(){
         return $this->makeVisible('is_admin')->is_admin;
