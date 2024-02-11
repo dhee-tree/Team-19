59d3ba87f304cc32c781d3fe65d8d0aa791c2286
220109796 <220109796@aston.ac.uk>
2024-02-11 02:32:41 +0000

    Fixed modal closing Added second modal window Worked with Umer on fixing modal Javascript

--- WoodLess/resources/views/components/users-info.blade.php
+++ WoodLess/resources/views/components/users-info.blade.php
@@ -1,111 +1,95 @@
-<div class="modal fade" id="ExtraModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-    <!-- Is responsible for expanding the fields for the item in the inventory management system -->
+<container id="ExtraModals">
+    <div class="modal fade" id="ExtraModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+        <!-- Is responsible for expanding the fields for the item in the inventory management system -->
 
-    <div class="modal-dialog modal-dialog-centered resizable">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="ExtraModalTitle">User Details</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="text-center mb-4">
-                    <!-- Assuming $user->image is a URL to the image -->
-                    @if($user->image)
-                    <img src="{{$user->image}}" alt="User Image">
-                    @else
-                    <!-- Default image if no user image available -->
-                    <img src="https://placehold.co/300x350" alt="Default User Image">
-                    @endif
-                    <!-- Display the user ID underneath the image with margin -->
-                    <p class="mt-3 mb-0">User ID: {{$user->id}}</p>
-                    <!-- Display the role underneath the image without a title -->
-                    <p><strong>{{$user->is_admin ? 'Admin' : 'Customer'}}</strong></p>
-                    <!-- Thin line -->
-                    <hr>
+        <div class="modal-dialog modal-dialog-centered resizable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ExtraModalTitle">User Details</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="row text-center">
-                    <div class="col-md-6">
-                        <p><strong>First name</strong><br>{{$user->first_name}}</p>
+                <div class="modal-body">
+                    <div class="text-center mb-4">
+                        <!-- Assuming $user->image is a URL to the image -->
+                        @if($user->image)
+                        <img src="{{$user->image}}" alt="User Image">
+                        @else
+                        <!-- Default image if no user image available -->
+                        <img src="https://placehold.co/300x350" alt="Default User Image">
+                        @endif
+                        <!-- Display the user ID underneath the image with margin -->
+                        <p class="mt-3 mb-0">User ID: {{$user->id}}</p>
+                        <!-- Display the role underneath the image without a title -->
+                        <p><strong>{{$user->is_admin ? 'Admin' : 'Customer'}}</strong></p>
+                        <!-- Thin line -->
+                        <hr>
                     </div>
-                    <div class="col-md-6">
-                        <p><strong>Last Name</strong><br>{{$user->last_name}}</p>
+                    <div class="row text-center">
+                        <div class="col-md-6">
+                            <p><strong>First name</strong><br>{{$user->first_name}}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Last Name</strong><br>{{$user->last_name}}</p>
 
-                    </div>
-                </div>
-                <div class="row text-center">
-                    <div class="col-md-6">
-                        <p><strong>Email</strong><br>{{$user->email}}</p>
-                    </div>
-                    <div class="col-md-6">
-                        <p><strong>Email verified</strong><br>
-                            @if($user->email_verified_at)
-                            <span style="color: green;">&#10004;</span>
-                            @else
-                            <span style="color: red;">&#10008;</span>
-                            @endif
-                        </p>
-                    </div>
-                    <div class="col-md-6">
-                        <p><strong>Phone number</strong><br>{{$user->phone_number}}</p>
-                    </div>
-                    <div class="col-md-6">
-                        <p><strong>Phone number</strong><br>{{$user->phone_number}}</p>
-                    </div>
-                    <div class="">
-                        <div class="d-flex justify-content-center">
-                            <button class="btn btn-primary me-3" data-bs-target="#cardsModal" data-bs-toggle="modal" data-bs-dismiss="modal">Cards</button>
-                            <button class="btn btn-primary ms-3" data-bs-target="#addressesModal" data-bs-toggle="modal" data-bs-dismiss="modal">Addresses</button>
                         </div>
                     </div>
+                    <div class="row text-center">
+                        <div class="col-md-6">
+                            <p><strong>Email</strong><br>{{$user->email}}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Email verified</strong><br>
+                                @if($user->email_verified_at)
+                                <span style="color: green;">&#10004;</span>
+                                @else
+                                <span style="color: red;">&#10008;</span>
+                                @endif
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Phone number</strong><br>{{$user->phone_number}}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p><strong>Phone number</strong><br>{{$user->phone_number}}</p>
+                        </div>
+                        <div class="">
+                            <div class="d-flex justify-content-center">
+                                <button class="btn btn-primary" data-bs-target="#CardsModal" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
+                                <button class="btn btn-primary ms-3" data-bs-target="#AddressesModal" data-bs-toggle="modal" data-bs-dismiss="modal">Addresses</button>
+                            </div>
+                        </div>
 
+                    </div>
+                    <br>
+                    <p class="text-center"><strong>Created</strong> {{date('F j, Y', strtotime($user->created_at))}}</p>
                 </div>
-                <br>
-                <p class="text-center"><strong>Created</strong> {{date('F j, Y', strtotime($user->created_at))}}</p>
+                <div class="modal-footer">
+                    <div class="col-auto">
+                        <button id="btn-close" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
             </div>
-            <div class="modal-footer">
-                <div class="col-auto">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+
+    <div class="modal fade" id="CardsModal" aria-hidden="true" aria-labelledby="ModalLabel2" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalLabel2">Modal 2</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Hide this modal and show the first with the button below.
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-bs-target="#ExtraModal" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
                 </div>
             </div>
-
         </div>
     </div>
-</div>
-
-<div class="modal fade" id="cardsModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Hide this modal and show the first with the button below.
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-target="#ExtraModal" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
-            </div>
-        </div>
-    </div>
-</div>
-<a class="btn btn-primary" data-bs-toggle="modal" href="#ExtraModal" role="button">Return</a>
+    <a class="btn btn-primary" data-bs-toggle="modal" href="#ExtraModal" role="button">Return</a>
 
 
-
-<div class="modal fade" id="cardsModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Hide this modal and show the first with the button below.
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-target="#ExtraModal" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
-            </div>
-        </div>
-    </div>
-</div>
-<a class="btn btn-primary" data-bs-toggle="modal" href="#ExtraModal" role="button">Return</a>
+</container>
--- WoodLess/resources/views/users-admin.blade.php
+++ WoodLess/resources/views/users-admin.blade.php
@@ -136,19 +136,41 @@
         $.get('/admin-panel/user-info/' + userId, function(data) {
             $('body').append(data);
             var modal = $('#ExtraModal');
+            var container = $('#ExtraModals');
             modal.modal('show'); // Show the modal after content is appended
 
-            // Remove the modal from the DOM when it's closed
-            modal.on('hidden.bs.modal', function() {
+            // Flag to determine if the ExtraModal was explicitly closed by the user
+            var extraModalClosed = false;
+
+
+            // Set the flag when the close button on the ExtraModal is clicked
+            modal.find('#btn-close').on('click', function() {
                 // Re-enable all buttons with the specified class when the modal is closed
                 var buttons = document.querySelectorAll(".openModalButton");
                 for (var i = 0; i < buttons.length; i++) {
                     buttons[i].disabled = false;
                 }
 
-                modal.remove();
+                container.remove();
+            });
+
+            // Event listener for clicking outside the modal
+            container.on('click', function(e) {
+                // Check if the click is outside the modal and not on any elements within the modal
+
+                if ($(e.target).hasClass('modal')) {
+                    // Re-enable all buttons with the specified class when the modal is closed
+                    var buttons = document.querySelectorAll(".openModalButton");
+                    for (var i = 0; i < buttons.length; i++) {
+                        buttons[i].disabled = false;
+                    }
+
+                    container.remove();
+                }
             });
         });
+
+
     }
 </script>
 @endsection
