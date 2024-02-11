c1ed81ceee934c8a81c12e919a63641f1afbde4e
Sgy157 <soggy706@googlemail.com>
2024-02-11 18:12:47 +0000

    Added pagination handling for Collection instances

--- WoodLess/app/Providers/AppServiceProvider.php
+++ WoodLess/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        if (!Collection::hasMacro('paginate')) {
+
+            Collection::macro('paginate', 
+                function ($perPage = 15, $page = null, $options = []) {
+                $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+                return (new LengthAwarePaginator(
+                    $this->forPage($page, $perPage), $this->count(), $perPage, $page, $options))
+                    ->withPath('');
+            });
+    }
     }
 }
