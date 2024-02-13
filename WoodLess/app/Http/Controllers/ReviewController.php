8d52c734bec2872950b1662cabb6ec115eea0f75
Sgy157 <soggy706@googlemail.com>
2024-02-13 14:04:18 +0000

    Added error checking to store method

--- WoodLess/app/Http/Controllers/ReviewController.php
+++ WoodLess/app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Review;
 use App\Models\Product;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -12,26 +13,34 @@ class ReviewController extends Controller
 
     //Store single review
     public function store(Request $request, Product $product){
-        //$user = auth()->user();
-        $product->loadMissing('reviews');
-        $user = auth()->user();
+        try {
+            $product->loadMissing('reviews');
+            $reviews = $product->reviews();
+            $user = auth()->user();
+    
+            $request->validate([
+                'user_id' => 'unique',
+                'rating' => 'required|min:1',
+                'description' => 'required|min:25'
+            ]);
+    
+            $reviews->create([
+                'user_id' => $user->id,
+                'rating' => $request->input('rating'),
+                'description' => $request->input('description')    
+            ]);
+    
+            return back()->with([
+                'status' => 'success',
+                'message' => 'Review uploaded successfully.'
+            ]);
 
-        $request->validate([
-            'user_id' => 'unique',
-            'rating' => 'required|min:1',
-            'description' => 'required|min:25'
-        ]);
-
-        $product->reviews()->create([
-            'user_id' => $user->id,
-            'rating' => $request->input('rating'),
-            'description' => $request->input('description')    
-        ]);
-
-        return back()->with([
-            'status' => 'success',
-            'message' => 'Review uploaded successfully.'
-        ]);
+        } catch (QueryException $e) {
+            return back()->with([
+                'status' => 'danger',
+                'message' => 'Failed to upload review.'
+            ]);
+        }
     }
 
     //Delete single review
