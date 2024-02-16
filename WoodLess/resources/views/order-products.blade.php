0971fb2ae1ab5355e0a4cc7acc54bf5e66300497
Ighomena Daniel Odebala <ighoodes@gmail.com>
2024-02-16 17:17:15 +0000

    Added condition for return button to be visible

--- WoodLess/resources/views/order-products.blade.php
+++ WoodLess/resources/views/order-products.blade.php
@@ -115,10 +115,7 @@
                                                 <p>£{{ $product->cost }}</p>
                                             </div>
                                             <div class="col-sm-3">
-                                                @if ($product->orderProductStatus->first()->status == 'Processing Return') 
-                                                    <span class="order-status order-status-processing order-small-width btn btn-warning disabled">{{ $product->orderProductStatus->first()->status }}</span>
-                                                    <i class="fa-solid fa-circle-xmark btn" style="color: #ff0000;" data-bs-toggle="tooltip" data-bs-placement="top" title="Cancel return"></i>
-                                                @else
+                                                @if ($product->orderProductStatus->first()->status == 'Complete') 
                                                     <a href="{{ route('user.return-purchase', ['order' => $order->id, 'product' => $product->id]) }}" class="btn btn-primary">Return Product</a>
                                                 @endif
                                             </div>
