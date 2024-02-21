a7c47e2f500136a2cd355d985d3201436e90e6ee
Umer Mohammed <D3m0nspider@gmail.com>
2024-02-21 22:58:24 +0000

    Fixed deleting products Added a self delete to the products model to allow custom deleting functionality.

--- WoodLess/app/Models/Product.php
+++ WoodLess/app/Models/Product.php
@@ -11,7 +11,17 @@ class Product extends Model
 {
     use HasFactory;
     use Cacheable;
-    
+
+    public static function boot(){
+        parent::boot();
+
+        self::deleted(function ($product) {
+            $product->categories()->delete();
+            $product->reviews()->delete();
+            $product->warehouses()->delete();
+        });
+    }
+
     protected $fillable = [
         'title',
         'description',
@@ -77,78 +87,79 @@ class Product extends Model
      * Returns the categories associated with the product.
      */
     public function categories()
-    {   
+    {
         return $this->belongsToMany(Category::class);
     }
 
     //filters the product
-  
-public function scopeFilter($query, array $filters)
-{
-    // Search
-    if ($searchText = $filters['search'] ?? false) {
-        $query->where(function ($searchQuery) use ($searchText) {
-            $searchQuery->where('title', 'like', '%' . $searchText . '%')
-                        ->orWhere('tags', 'like', '%' . $searchText . '%');
-        });
-    }
 
-    // Category
-    if ($category = $filters['categories'] ?? false) {
-        $query->whereHas('categories', function ($categoryQuery) use ($category) {
-            $categoryQuery->where('category', $category);
-        });
-    }
-
-    // Ratings
-    if ($ratings = $filters['ratings'] ?? false) {
-        $query->whereHas('reviews', function ($reviewQuery) use ($ratings) {
-            foreach ($ratings as $rating) {
-                $reviewQuery->havingRaw('coalesce(avg(rating), 0) >= ?', [$rating]);
-            }
-        });
-    }
-
-    // Color
-    if ($color = $filters['color'] ?? false) {
-        $query->whereJsonContains('attributes->color', $color);
-    }
-
-    // Price
-    if (isset($filters['minCost'])) {
-        $query->where('cost', '>=', $filters['minCost']);
-    }
-
-    if (isset($filters['maxCost'])) {
-        $query->where('cost', '<=', $filters['maxCost']);
-    }
-
-    // Sorting
-    if (isset($filters['sort_by'])) {
-        $sortBy = $filters['sort_by'];
-        if ($sortBy === 'price_high_low') {
-            $query->orderByDesc('cost');
-        } elseif ($sortBy === 'price_low_high') {
-            $query->orderBy('cost');
-        } elseif ($sortBy === 'rating_high_low') {
-            $query->orderByDesc('rating'); 
-        } elseif ($sortBy === 'rating_low_high') {
-            $query->orderBy('rating'); 
-        } elseif ($sortBy === 'discount_high_low') {
-            $query->orderByDesc('discount');
-        } elseif ($sortBy === 'discount_low_high') {
-            $query->orderBy('discount');
+    public function scopeFilter($query, array $filters)
+    {
+        // Search
+        if ($searchText = $filters['search'] ?? false) {
+            $query->where(function ($searchQuery) use ($searchText) {
+                $searchQuery->where('title', 'like', '%' . $searchText . '%')
+                    ->orWhere('tags', 'like', '%' . $searchText . '%');
+            });
         }
-    }
 
-    return $query;
-}
+        // Category
+        if ($category = $filters['categories'] ?? false) {
+            $query->whereHas('categories', function ($categoryQuery) use ($category) {
+                $categoryQuery->where('category', $category);
+            });
+        }
+
+        // Ratings
+        if ($ratings = $filters['ratings'] ?? false) {
+            $query->whereHas('reviews', function ($reviewQuery) use ($ratings) {
+                foreach ($ratings as $rating) {
+                    $reviewQuery->havingRaw('coalesce(avg(rating), 0) >= ?', [$rating]);
+                }
+            });
+        }
+
+        // Color
+        if ($color = $filters['color'] ?? false) {
+            $query->whereJsonContains('attributes->color', $color);
+        }
+
+        // Price
+        if (isset($filters['minCost'])) {
+            $query->where('cost', '>=', $filters['minCost']);
+        }
+
+        if (isset($filters['maxCost'])) {
+            $query->where('cost', '<=', $filters['maxCost']);
+        }
+
+        // Sorting
+        if (isset($filters['sort_by'])) {
+            $sortBy = $filters['sort_by'];
+            if ($sortBy === 'price_high_low') {
+                $query->orderByDesc('cost');
+            } elseif ($sortBy === 'price_low_high') {
+                $query->orderBy('cost');
+            } elseif ($sortBy === 'rating_high_low') {
+                $query->orderByDesc('rating');
+            } elseif ($sortBy === 'rating_low_high') {
+                $query->orderBy('rating');
+            } elseif ($sortBy === 'discount_high_low') {
+                $query->orderByDesc('discount');
+            } elseif ($sortBy === 'discount_low_high') {
+                $query->orderBy('discount');
+            }
+        }
+
+        return $query;
+    }
 
     /**
      * Returns the order status associated with the product.
      * Rename to orderStatus to match model if possible
      */
-    public function orderProductStatus(){
+    public function orderProductStatus()
+    {
         return $this->belongsToMany(OrderStatus::class, 'order_product_warehouse', 'product_id', 'status_id');
     }
 
@@ -172,6 +183,4 @@ public function scopeFilter($query, array $filters)
 
         return $truncatedDescription;
     }
-
-
 }
