e2eda55e3a74286243a69d70cba39c1e587ed3b9
Zaakir04 <220140434@aston.ac.uk>
2024-02-22 12:20:42 +0000

    Coded the user panel base file and created the base css file to accomodate

--- WoodLess/resources/views/user-panel.blade.php
+++ WoodLess/resources/views/user-panel.blade.php
@@ -1,63 +1,6 @@
-@extends('layouts.base')
-@section('title', 'WoodLess - User Panel')
+@extends('layouts.user-panel-base')
 
-@section('style')
-<link rel="stylesheet" href="{{ asset('css/user-panel.css')}}">
-@endsection
-
-@section('content')
-    <!-- Code for sidebar -->
-    @if (session()->has('success'))
-        <div class="alert alert-success">
-            <i class="fa-regular fa-circle-check fa-fade"></i> <span>{{ session('success') }}</span>
-        </div>
-    @endif
-    <div class="wrapper">
-
-        <aside id="sidebar">
-            <div class="sidebar-list">
-                <div class="heading">
-                    <p>Hi, Welcome back to your account.</p>
-                </div>
-                <ul class="sidebar">
-                    <li class="sidebar-item">
-                        <a href="{{ route('user-panel') }}" class="sidebar-link">
-                            <i class="fa-regular fa-clipboard-list pe-2"></i>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{ route('user.purchases') }}" class="sidebar-link">
-                            <i class="fa-regular fa-clipboard-list pe-2"></i>
-                            My Purchases
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{url('/user-panel/tickets')}}" class="sidebar-link">
-                            <i class="fa-regular fa-clipboard-list pe-2"></i>
-                            Tickets
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{ route('password.change.form') }}" class="sidebar-link">
-                            <i class="fa-solid fa-lock" style="color: #e8e8e8;"></i>
-                            Change Password
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{ route('logout') }}" class="sidebar-link"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                            <i class="fa-solid fa-arrow-right-from-bracket" style="color:#e8e8e8; margin-right:10px;"></i> Logout
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
-            </div>
-        </aside>
-
+@section('other')
         <!-- Main content -->
         <div class="main content">
             <!-- Code for banner -->
@@ -135,9 +78,6 @@
                     </div>
                 </div>
             </section>
-
-
-
         </div>
     </div>
 @endsection
