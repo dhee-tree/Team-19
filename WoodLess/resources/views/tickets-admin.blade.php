2bdb2c80964fcca773a16d1919974e4adb4a3c97
NduNdu21 <ndumisobm@icloud.com>
2024-02-26 03:08:42 +0000

    updated the tickets page

--- WoodLess/resources/views/tickets-admin.blade.php
+++ WoodLess/resources/views/tickets-admin.blade.php
@@ -6,135 +6,115 @@
 @endsection
 
 @section('content')
+    <!-- sidebar -->
+    <div class="sidebar">
+        <a href="#" class="logo">
+            <div class="logo-name"><span>Wood</span>Less</div>
+        </a>
+        <ul class="side-menu">
+            <li><a href="{{url('/admin-panel')}}"><i class="fa-solid fa-house"></i>Dashboard</a></li>
+            <li><a href="{{url('/admin-panel/inventory')}}"><i class="fa-solid fa-warehouse"></i>Inventory</a></li>
+            <li><a href="{{url('/admin-panel/orders')}}"><i class="fa-solid fa-truck-moving"></i>Orders</a></li>
+            <li class="active"><a href="{{url('/admin-panel/tickets')}}"><i class="fa-solid fa-message"></i>Tickets</a></li>
+            <li><a href="{{url('/admin-panel/users')}}"><i class="fa-solid fa-user"></i>Users</a></li>
+        </ul>
+        <ul class="side-menu">
+            <li>
+                <a href="#" class="logout">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- End of sidebar -->
 
-<div class="wrapper">
-    <aside id="sidebar">
-        <!-- Content for sidebar -->
-        <div class="h-100">
-            <div class="sidebar-logo">
-                <a href="#">Admin Panel</a>
-            </div>
-            <ul class="sidebar-nav">
-                <li class="sidebar-item">
-                    <a href="{{url('admin-panel')}}" class="sidebar-link">
-                        <i class="fa-solid fa-house pe-2"></i>
-                        Dashboard
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="{{url('/admin-panel/orders')}}" class="sidebar-link">
-                        <i class="fa-solid fa-list pe-2"></i>
-                        Orders
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link collapsed" data-bs-target="#customers" data-bs-toggle="collapse" aria-expanded="false">
-                        <i class="fa-solid fa-user pe-2"></i>
-                        Customers
-                    </a>
-                    <ul id="customers" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                        <li class="sidebar-item">
-                            <a href="{{url('/admin-panel/users')}}" class="sidebar-link">User display</a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="{{url('/admin-panel/tickets')}}" class="sidebar-link">Support tickets</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="sidebar-item">
-                    <a href="{{url('/admin-panel/inventory')}}" class="sidebar-link">
-                        <i class="fa-regular fa-store pe-2"></i>
-                        Inventory
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
-    <div class="main">
-        <div class="container-fluid px-5 py-4">
-            <div class="row pb-3">
-                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <p class="text-sm mb-0 text-uppercase">ALL TICKETS</p>
-                            <h4 class="font-weight-bolder">
-                                <!-- store number of tickets here, will hardcode a value for now -->
-                                230
-                            </h4>
+    <div class="content">
+        <div class="main">
+            <div class="container-fluid px-5 py-4">
+                <div class="row pb-3">
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <p class="text-sm mb-0 text-uppercase">ALL TICKETS</p>
+                                <h4 class="font-weight-bolder">
+                                    <!-- store number of tickets here, will hardcode a value for now -->
+                                    230
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <p class="text-sm mb-0 text-uppercase">SOLVED TICKETS</p>
+                                <h4 class="font-weight-bolder">
+                                    <!-- store number of tickets here, will hardcode a value for now -->
+                                    60
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                        <div class="card">
+                            <div class="card-body p-3">
+                                <p class="text-sm mb-0 text-uppercase">PENDING TICKETS</p>
+                                <h4 class="font-weight-bolder">
+                                    <!-- store number of tickets here, will hardcode a value for now -->
+                                    170
+                                </h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <p class="text-sm mb-0 text-uppercase">SOLVED TICKETS</p>
-                            <h4 class="font-weight-bolder">
-                                <!-- store number of tickets here, will hardcode a value for now -->
-                                60
-                            </h4>
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="dataTables_length" id="user_length">
+                            <label>
+                                Show
+                                <select name="user_length" id="user_length" aria-controls="example" class="form-select form-select-sm">
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                                entries
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="dataTables_filter" id="user_filter">
+                            <label>
+                                Search:
+                                <input type="search" class="form-control form-control-sm" placeholder aria-controls="search">
+                            </label>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <p class="text-sm mb-0 text-uppercase">PENDING TICKETS</p>
-                            <h4 class="font-weight-bolder">
-                                <!-- store number of tickets here, will hardcode a value for now -->
-                                170
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <div class="dataTables_length" id="user_length">
-                        <label>
-                            Show
-                            <select name="user_length" id="user_length" aria-controls="example" class="form-select form-select-sm">
-                                <option value="10">10</option>
-                                <option value="25">25</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                            </select>
-                            entries
-                        </label>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="dataTables_filter" id="user_filter">
-                        <label>
-                            Search:
-                            <input type="search" class="form-control form-control-sm" placeholder aria-controls="search">
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="card mb-4">
-                <div class="card-body px-0 pt-0 pb-2">
-                    <div class="datatable table-responsive datatable-inner ps">
-                        <table class="table datatable-table align-items-center mb-0">
-                            <thead class="datatable">
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Ticket ID</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Ticket title</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Message</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Date</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">User</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody class="datatable-body">
-                                <tr scope="row">
+                <div class="card mb-4">
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="datatable table-responsive datatable-inner ps">
+                            <table class="table datatable-table align-items-center mb-0">
+                                <thead class="datatable">
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Ticket ID</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Ticket title</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Message</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Date</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">User</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" scope="col">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="datatable-body">
+                                    <tr scope="row">
 
-                                </tr>
-                            </tbody>
-                        </table>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
