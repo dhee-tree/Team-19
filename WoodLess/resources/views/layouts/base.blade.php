3f4f3ecbf571e8b65781ce52781835513451ea1a
220109796 <220109796@aston.ac.uk>
2024-02-28 00:27:42 +0000

    Fixed logo href

--- WoodLess/resources/views/layouts/base.blade.php
+++ WoodLess/resources/views/layouts/base.blade.php
@@ -35,7 +35,7 @@
 <body class="antialiased">
     <nav id="navbar" class="navbar navbar-expand-lg sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" style="margin-left:4px" href="#"><img class="logo" src="{{ asset('images\logo.png') }}" alt="Woodless Logo" /></a>
+            <a class="navbar-brand" style="margin-left:4px" href="/"><img class="logo" src="{{ asset('images\logo.png') }}" alt="Woodless Logo" /></a>
 
 
             <li id="iconsnavbar" class="d-flex ms-2">
