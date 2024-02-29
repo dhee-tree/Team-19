fa12665d9902da21553e9476c124fc384ac0bd3e
Sgy157 <soggy706@googlemail.com>
2024-02-29 15:15:07 +0000

    Updated margin

--- WoodLess/resources/views/products/show.blade.php
+++ WoodLess/resources/views/products/show.blade.php
@@ -23,7 +23,7 @@
 
 @section('content')
     <div id="maincontent" class="container">
-        <div class="row m-0 mt-3 px-1" id="product-main">
+        <div class="row m-0 mt-4 px-1" id="product-main">
             <div class="col-md-6 mb-3" id="gallery">
                 <div id="productGallery" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                     <div class="carousel-inner">
