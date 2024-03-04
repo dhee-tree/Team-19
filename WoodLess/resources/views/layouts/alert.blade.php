ac46cc4d766d9ac0244d3ace078663f170587ccb
Sgy157 <soggy706@googlemail.com>
2024-03-04 13:07:03 +0000

    Updated alert, now in center.
    The support button was blocking it. Also changed admin version.

--- WoodLess/resources/views/layouts/alert.blade.php
+++ WoodLess/resources/views/layouts/alert.blade.php
@@ -1,29 +1,29 @@
-@if(session('status'))
-<div class="container" style="margin-top: -20px">
-    <div class="mt-4 mb-2 alert alert-{{session('status')}} alert-dismissible fade show" role="alert">
-        @switch(session('status'))
-            @case('success')
-                <i class="fa-solid fa-xs fa-check"></i>
-            @break
-    
-            @case('warning')
-            <i class="fa-solid fa-warning"></i>
-            @break
-    
-            @case('danger')
-                <i class="fa-solid fa-xmark"></i>
-            @break
-    
-            @case('info')
-                <i class="fa-solid fa-circle-info"></i>
-            @break  
-    
-            @default
-                <i class="fa-solid fa-circle-info"></i>
-            @break
-        @endswitch
-        {{session('message')}}
-        <button type="button" class="btn-close btn-sm" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
+@if (session('status'))
+<div id="successAlert"
+    class="alert z-3 alert-{{ session('status') ?? 'info' }} fade show shadow-sm text-center position-fixed translate-middle bottom-0 start-50 mb-2 py-2"
+    role="alert">
+    @switch(session('status'))
+        @case('success')
+            <i class="fa-solid fa-xs fa-check"></i>
+        @break
+
+        @case('warning')
+            <i class="fa-solid fa-xs fa-warning"></i>
+        @break
+
+        @case('danger')
+            <i class="fa-solid fa-xs fa-xmark"></i>
+        @break
+
+        @case('info')
+            <i class="fa-solid fa-xs fa-circle-info"></i>
+        @break
+
+        @default
+            <i class="fa-solid fa-xs fa-circle-info"></i>
+        @break
+    @endswitch
+    {{ session('message') }}
+    <button hidden type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
