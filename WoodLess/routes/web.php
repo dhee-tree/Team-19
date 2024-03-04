8edea61111fca225de86809f909c6c7595d8e4ce
Ismaeel Noor <220103820@aston.ac.uk>
2024-03-04 18:04:55 +0000

    Forgot password working

--- WoodLess/routes/web.php
+++ WoodLess/routes/web.php
@@ -189,7 +189,7 @@ Route::post('/forgot-password', function (Request $request) {
 })->middleware('guest')->name('password.email');
 //reset password form after passwords forgot
 Route::get('/reset-password/{token}', function (string $token) {
-    return view('auth.reset-password', ['token' => $token]);
+    return view('auth.passwords.reset', ['token' => $token]);
 })->middleware('guest')->name('password.reset');
 
 Route::post('/reset-password', function (Request $request) {
