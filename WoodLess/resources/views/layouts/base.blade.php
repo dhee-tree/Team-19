af3f8d196d50c4c77800085484a1eff9bfbde391
Ismaeel Noor <220103820@aston.ac.uk>
2024-03-04 18:29:01 +0000

    Added live chat

--- WoodLess/resources/views/layouts/base.blade.php
+++ WoodLess/resources/views/layouts/base.blade.php
@@ -8,6 +8,8 @@
     <meta name="author"
         content="Ighomena Odebala, Lewis Neiland, Zaakir Mohammad, Ismaeel Noor, Ndumiso Mbangeleli, Abdulhamid Mustapha, Umer Mohammed, Matteo Crozat">
     <meta name="keywords" content="team project, birmingham, aston university">
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
     <title>@yield('title')</title>
 
@@ -71,15 +73,15 @@
                     </a>
                 @endguest
 
-                <button id="navbartoggler" class="d-lg-none btn btn-outline-light rounded-pill ms-2" type="button" data-bs-toggle="collapse"
+                <button id="navbartoggler" class="navbar-toggler ms-2" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false"
                     aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
             </li>
 
             <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav me-auto mb-1 ms-2 mb-lg-0 ms-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page"
                             href="{{ url('/') }}">Home</a>
@@ -102,15 +104,14 @@
                     </li>
                 </ul>
 
-                <form method="GET" action="{{ route('products.filter') }}" class="d-flex justify-content-lg-end justify-content-sm-center" role="search">
-                    <input type="search" name="search" placeholder="Search Products..." class="rounded-pill form-control search-input" />
+                <form method="GET" action=" {{ route('products.filter') }}" class="d-flex justify-content-center"
+                    role="search">
+                    <input type="search" name="search" placeholder="Search Products..."
+                        class="rounded-pill form-control" />
                     <button class="ms-2 btn btn-outline-light rounded-pill" type="submit" data-mdb-ripple-init>
                         <i class="fa-solid fa-magnifying-glass"></i>
                     </button>
                 </form>
-                
-
-
             </div>
 
 
@@ -120,7 +121,7 @@
 
     <div class="col" id="basket-offcanvas-div" tabindex="-1">
         <div class="shadow-lg offcanvas offcanvas-end scrollable" id="basket-offcanvas" aria-labelledby="basket-offcanvas">
-            <div class="offcanvas-header text-light mb-0 py-3">
+            <div class="offcanvas-header text-light mb-0 py-3" style="background-color: #1d1912">
               <span class="fs-5 offcanvas-title" id="basket-offcanvas">Your Basket</span>
               <button type="button" class="btn text-light" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
             </div>
@@ -176,12 +177,12 @@
 
                     <div class="col bg-white pt-2 sticky-bottom">
                         <div class="row-100 mb-0">
-                            <h5 class="fw-bold">Total: £{{sprintf("%0.2f",$totalBasketCost)}}</h5>
+                            <h5 class="fw-bold">Total: £{{$totalBasketCost}}</h5>
                         </div>
                         <div class="d-flex justify-content-around">
                             <a style="background-color: #1d1912" class="flex-fill btn text-light me-2" role="button" href="{{asset('basket')}}">
                                 Edit Basket
-                                <span class="ms-1 fw-light badge rounded-pill badge-notification">
+                                <span style="background-color: #655d52" class="ms-1 fw-light badge rounded-pill badge-notification">
                                     {{$user->basket()->first()->productAmount()}}
                                 </span>
                             </a>
@@ -211,7 +212,7 @@
     <!-- Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Create a support ticket</h1>
@@ -219,7 +220,195 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form action="{{ route('user.tickets.store') }}" method="POST">
+                <div class="card card-danger direct-chat direct-chat-danger">
+  <div class="card-header">
+    <h3 class="card-title">Direct Chat</h3>
+    <div class="card-tools">
+      <span data-toggle="tooltip" title="3 New Messages" class="badge badge-light">3</span>
+      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+        <i class="fas fa-minus"></i>
+      </button>
+      <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
+        <i class="fas fa-comments"></i>
+      </button>
+      <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
+  <!-- /.card-header -->
+  <div class="card-body">
+    <!-- Conversations are loaded here -->
+    <div class="direct-chat-messages">
+      <!-- Message. Default to the left -->
+      <div class="direct-chat-msg">
+        <div class="direct-chat-infos clearfix">
+          <span class="direct-chat-name float-left">Alexander Pierce</span>
+          <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+        </div>
+        <!-- /.direct-chat-infos -->
+        <img class="direct-chat-img" src="/docs/3.2/assets/img/user1-128x128.jpg" alt="message user image">
+        <!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          Is this template really for free? That's unbelievable!
+        </div>
+        <!-- /.direct-chat-text -->
+      </div>
+      <!-- /.direct-chat-msg -->
+      <!-- Message to the right -->
+      <div class="direct-chat-msg right">
+        <div class="direct-chat-infos clearfix">
+          <span class="direct-chat-name float-right">Sarah Bullock</span>
+          <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
+        </div>
+        <!-- /.direct-chat-infos -->
+        <img class="direct-chat-img" src="/docs/3.2/assets/img/user3-128x128.jpg" alt="message user image">
+        <!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          You better believe it!
+        </div>
+        <!-- /.direct-chat-text -->
+      </div>
+      <!-- /.direct-chat-msg -->
+      <!-- Message. Default to the left -->
+      <div class="direct-chat-msg">
+        <div class="direct-chat-infos clearfix">
+          <span class="direct-chat-name float-left">Alexander Pierce</span>
+          <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
+        </div>
+        <!-- /.direct-chat-infos -->
+        <img class="direct-chat-img" src="/docs/3.2/assets/img/user1-128x128.jpg" alt="message user image">
+        <!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          Working with AdminLTE on a great new app! Wanna join?
+        </div>
+        <!-- /.direct-chat-text -->
+      </div>
+      <!-- /.direct-chat-msg -->
+      <!-- Message to the right -->
+      <div class="direct-chat-msg right">
+        <div class="direct-chat-infos clearfix">
+          <span class="direct-chat-name float-right">Sarah Bullock</span>
+          <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
+        </div>
+        <!-- /.direct-chat-infos -->
+        <img class="direct-chat-img" src="/docs/3.2/assets/img/user3-128x128.jpg" alt="message user image">
+        <!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          I would love to.
+        </div>
+        <!-- /.direct-chat-text -->
+      </div>
+      <!-- /.direct-chat-msg -->
+    </div>
+    <!--/.direct-chat-messages-->
+    <!-- Contacts are loaded here -->
+    <div class="direct-chat-contacts">
+      <ul class="contacts-list">
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user1-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Count Dracula
+                <small class="contacts-list-date float-right">2/28/2015</small>
+              </span>
+              <span class="contacts-list-msg">How have you been? I was...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user7-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Sarah Doe
+                <small class="contacts-list-date float-right">2/23/2015</small>
+              </span>
+              <span class="contacts-list-msg">I will be waiting for...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user3-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Nadia Jolie
+                <small class="contacts-list-date float-right">2/20/2015</small>
+              </span>
+              <span class="contacts-list-msg">I'll call you back at...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user5-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Nora S. Vans
+                <small class="contacts-list-date float-right">2/10/2015</small>
+              </span>
+              <span class="contacts-list-msg">Where is your new...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user6-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                John K.
+                <small class="contacts-list-date float-right">1/27/2015</small>
+              </span>
+              <span class="contacts-list-msg">Can I take a look at...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="/docs/3.2/assets/img/user8-128x128.jpg">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Kenneth M.
+                <small class="contacts-list-date float-right">1/4/2015</small>
+              </span>
+              <span class="contacts-list-msg">Never mind I found...</span>
+            </div>
+            <!-- /.contacts-list-info -->
+          </a>
+        </li>
+        <!-- End Contact Item -->
+      </ul>
+      <!-- /.contacts-list -->
+    </div>
+    <!-- /.direct-chat-pane -->
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+    <form action="#" method="post">
+      <div class="input-group">
+        <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+        <span class="input-group-append">
+          <button type="button" class="btn btn-primary">Send</button>
+        </span>
+      </div>
+    </form>
+  </div>
+  <!-- /.card-footer-->
+</div>
+<!--/.direct-chat -->
+                    <!-- form for creating a ticket -->
+                    <!-- <form action="{{ route('user.tickets.store') }}" method="POST">
                         @csrf
                         <div class="modal-body">
 
@@ -235,7 +424,7 @@
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                             <button type="submit" class="btn btn-primary">Create Ticket</button>
                         </div>
-                    </form>
+                    </form> -->
 
                 </div>
             </div>
@@ -316,15 +505,43 @@
 
 
     @if (session('success'))
-        <div id="successAlert" class="alert fade show alert-success shadow-sm text-center position-fixed translate-middle bottom-0 start-50 mb-2 py-2" role="alert"
+        <div id="successAlert" class="alert alert-success position-fixed bottom-0 end-0 mb-3 me-3 z-3" role="alert"
             style="display: none;">
 
             {{ session('success') }}
-            <button hidden type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <button type="button" class="btn-close" aria-label="Close"></button>
         </div>
     @endif
 
-    @include('layouts.alert')
+    @if (session('status'))
+        <div id="successAlert"
+            class="alert z-3 alert-{{ session('status') ?? 'info' }} fade show position-fixed bottom-0 end-0 mb-3 me-3 py-2"
+            role="alert">
+            @switch(session('status'))
+                @case('success')
+                    <i class="fa-solid fa-xs fa-check"></i>
+                @break
+
+                @case('warning')
+                    <i class="fa-solid fa-xs fa-warning"></i>
+                @break
+
+                @case('danger')
+                    <i class="fa-solid fa-xs fa-xmark"></i>
+                @break
+
+                @case('info')
+                    <i class="fa-solid fa-xs fa-circle-info"></i>
+                @break
+
+                @default
+                    <i class="fa-solid fa-xs fa-circle-info"></i>
+                @break
+            @endswitch
+            {{ session('message') }}
+            <!--<button type="button" class="btn-close btn-sm py-0" data-bs-dismiss="alert" aria-label="Close"></button>!-->
+        </div>
+    @endif
     <!-- bootstrap 5.3 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
