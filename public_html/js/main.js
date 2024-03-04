1fb863fe5512eaaf1a5f8bdc6e2ef3faaed5be3a
Sgy157 <soggy706@googlemail.com>
2024-03-04 13:06:01 +0000

    Alert now uses bs fade instead of disspearing

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,15 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Hide the alert after 5 seconds (adjust as needed)
         setTimeout(function () {
-            successAlert.style.display = 'none';
+            successAlert.classList.remove('show');
         }, 5000); // 5000 milliseconds = 5 seconds
-
-        // Close the alert when the close button is clicked
-        var closeButton = successAlert.querySelector('.btn-close');
-        if (closeButton) {
-            closeButton.addEventListener('click', function () {
-                successAlert.style.display = 'none';
-            });
-        }
     }
 });
