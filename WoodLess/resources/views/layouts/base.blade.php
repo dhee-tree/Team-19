0b83873ae03d7c576780f325164cf34e90690a0a
Sgy157 <soggy706@googlemail.com>
2024-03-04 13:05:33 +0000

    Moved (Lewis') alert back to alert file.

--- WoodLess/resources/views/layouts/base.blade.php
+++ WoodLess/resources/views/layouts/base.blade.php
@@ -316,43 +316,15 @@
 
 
     @if (session('success'))
-        <div id="successAlert" class="alert alert-success position-fixed bottom-0 end-0 mb-3 me-3 z-3" role="alert"
+        <div id="successAlert" class="alert fade show alert-success shadow-sm text-center position-fixed translate-middle bottom-0 start-50 mb-2 py-2" role="alert"
             style="display: none;">
 
             {{ session('success') }}
-            <button type="button" class="btn-close" aria-label="Close"></button>
+            <button hidden type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
 
-    @if (session('status'))
-        <div id="successAlert"
-            class="alert z-3 alert-{{ session('status') ?? 'info' }} fade show position-fixed bottom-0 end-0 mb-3 me-3 py-2"
-            role="alert">
-            @switch(session('status'))
-                @case('success')
-                    <i class="fa-solid fa-xs fa-check"></i>
-                @break
-
-                @case('warning')
-                    <i class="fa-solid fa-xs fa-warning"></i>
-                @break
-
-                @case('danger')
-                    <i class="fa-solid fa-xs fa-xmark"></i>
-                @break
-
-                @case('info')
-                    <i class="fa-solid fa-xs fa-circle-info"></i>
-                @break
-
-                @default
-                    <i class="fa-solid fa-xs fa-circle-info"></i>
-                @break
-            @endswitch
-            {{ session('message') }}
-            <!--<button type="button" class="btn-close btn-sm py-0" data-bs-dismiss="alert" aria-label="Close"></button>!-->
-        </div>
-    @endif
+    @include('layouts.alert')
     <!-- bootstrap 5.3 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
