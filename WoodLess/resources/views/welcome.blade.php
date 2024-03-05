d4e1ccb9f5ce9658055e79df2245c485e54db636
Umer Mohammed <D3m0nspider@gmail.com>
2024-03-05 22:19:53 +0000

    Removed testing o the bedroom styling

--- WoodLess/resources/views/welcome.blade.php
+++ WoodLess/resources/views/welcome.blade.php
@@ -229,7 +229,7 @@
                         </div>
                         <button class="button" id="bedroom">
                             <h3>
-                                Bedroom asda Testings
+                                Bedroom
                             </h3>
 
                             <p class="info-text">Browse</p>
