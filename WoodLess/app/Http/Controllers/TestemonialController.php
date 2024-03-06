d4b8d34815713bc8bbefd7ff48b976b6abc48c34
Ismaeel Noor <220103820@aston.ac.uk>
2024-03-06 14:18:00 +0000

    added logic for the controller for tetsemonials and created a model

--- WoodLess/app/Http/Controllers/TestemonialController.php
+++ WoodLess/app/Http/Controllers/TestemonialController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Testimonial;
 
 class TestemonialController extends Controller
 {
+    public function store(Request $request)
+    {
+        // Validate the form data
+        $request->validate([
+            'rating' => 'required|integer|min:1|max:5',
+            'opinion' => 'required|string|max:255',
+        ]);
+
+        // Create a new testimonial
+        $testimonial = new Testimonial();
+        $testimonial->rating = $request->rating;
+        $testimonial->description = $request->opinion;
+        $testimonial->user_id = auth()->id(); 
+        $testimonial->save();
+
+       
+        return redirect()->back()->with('success', 'Thank you for your review!');
+    }
     //
 }
