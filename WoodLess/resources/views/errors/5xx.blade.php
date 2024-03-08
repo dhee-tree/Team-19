f8d223a6c0080a7a1a2b3939bf90eca4c00c41b6
Sgy157 <soggy706@googlemail.com>
2024-03-08 16:25:04 +0000

    Added additional error code

--- WoodLess/resources/views/errors/5xx.blade.php
+++ WoodLess/resources/views/errors/5xx.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.base')
 
-@section('title', 'WoodLess - Error')
+@section('title', 'WoodLess - {{$exception->getStatusCode()}}')
 @section('content')
 <div id="maincontent" class="container">
-    <h1>{{$exception->getStatusCode()}}: {{$exception->getMessage()}}</h1>
+    <h1>{{$exception->getStatusCode()}}: {{(empty($exception->getMessage())) ? 'Something went wrong' : $exception->getMessage()}}.</h1>
     <p>Back to <a href="{{url('/products')}}">products</a></p>
 </div>
 @endsection
