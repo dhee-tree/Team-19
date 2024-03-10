980742682e41c62f7bb136f4f2225c992c573645
Zaakir04 <220140434@aston.ac.uk>
2024-03-10 17:33:27 +0000

    Updated lost route

--- WoodLess/routes/web.php
+++ WoodLess/routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\AddressController;
 
 /*
 |--------------------------------------------------------------------------
