bdf453ce205d3c607e961cf9c2c33734f90980f2
Ismaeel Noor <220103820@aston.ac.uk>
2024-03-10 02:04:22 +0000

    added the same styling for the product and return flow

--- public_html/js/livechat.js
+++ public_html/js/livechat.js
@@ -330,7 +330,16 @@ function showSecondProductChat(chat) {
 
     if (chat === "product-chat-1") {
         var order = document.getElementById("product-response-1");
+        
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+
         setTimeout(function() {
+            // Hide loader and display AI response
+            loader.style.display = 'none';
             order.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
 
@@ -345,15 +354,30 @@ function showSecondProductChat(chat) {
         // Show next user option
         var order_chat_1 = document.getElementById("product-chat-5");
         var order_chat_2 = document.getElementById("product-chat-6");
+
         setTimeout(function() {
             order_chat_1.style.display = 'block';
             order_chat_2.style.display = 'block';
             order_chat_1.style.marginTop = '120px';
         }, 2500);
-    } 
+    }
+
+
     else if (chat === "product-chat-2") {
         var product = document.getElementById("product-response-2");
-        product.style.display = 'block';
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        product.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        setTimeout(function() {
+            // Hide loader and display AI response
+            loader.style.display = 'none';
+            product.style.display = 'block';
+        }, 2000); // 2000 milliseconds = 2 seconds
+    
         // Show next user option
         var order_chat_1 = document.getElementById("product-chat-5");
         var order_chat_2 = document.getElementById("product-chat-6");
@@ -362,19 +386,31 @@ function showSecondProductChat(chat) {
             order_chat_2.style.display = 'block';
             order_chat_1.style.marginTop = '120px';
         }, 2500);
-
+    
         var order = document.getElementById("product-chat-1");
         order.style.display = 'none';
-
+    
         var return1 = document.getElementById("product-chat-3");
         return1.style.display = 'none';
         var shush = document.getElementById("product-chat-4");
         shush.style.display = 'none';
-    } 
+    }
+    
     else if (chat === "product-chat-3") {
         var product = document.getElementById("product-response-3");
-        var product = document.getElementById("product-response-3");
-        product.style.display = 'block';
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        product.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        setTimeout(function() {
+            // Hide loader and display AI response
+            loader.style.display = 'none';
+            product.style.display = 'block';
+        }, 2000); // 2000 milliseconds = 2 seconds
+    
         // Show next user option
         var order_chat_1 = document.getElementById("product-chat-5");
         var order_chat_2 = document.getElementById("product-chat-6");
@@ -383,79 +419,117 @@ function showSecondProductChat(chat) {
             order_chat_2.style.display = 'block';
             order_chat_1.style.marginTop = '120px';
         }, 2500);
+    
         var order = document.getElementById("product-chat-1");
         order.style.display = 'none';
-
+    
         var return1 = document.getElementById("product-chat-2");
         return1.style.display = 'none';
         var shush = document.getElementById("product-chat-4");
         shush.style.display = 'none';
-    } else {
+    }
+    
+    else {
         var shush = document.getElementById("product-response-4");
         var form_response = document.getElementById("form-response");
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        shush.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
         setTimeout(function() {
+            // Hide loader and display AI response and form response
+            loader.style.display = 'none';
             shush.style.display = 'block';
             form_response.style.display = 'block';
-        }, 2500);
+        }, 2500); // 2500 milliseconds = 2.5 seconds
+    
         var product = document.getElementById("product-chat-2");
         var return1 = document.getElementById("product-chat-3");
         var order = document.getElementById("product-chat-1");
-
+    
         product.style.display = 'none';
         return1.style.display = 'none';
-        order.style.display = 'none';       
+        order.style.display = 'none';
     }
+    
 }
 function showSecondProductChat2(chat) {
     var chat_button = document.getElementById(chat);
     disablePreviousChat(chat_button);
     if (chat === "product-chat-5") { // yes
         var order = document.getElementById("product-response-5");
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
         setTimeout(function() {
+            // Hide loader and display AI response
+            loader.style.display = 'none';
             order.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
-
+    
         var product = document.getElementById("product-chat-6");
         product.style.display = 'none';
-    } 
+    }
     else { // no
         var order = document.getElementById("product-response-6");
         var form_response = document.getElementById("form-response");
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
         setTimeout(function() {
+            // Hide loader and display AI response
+            loader.style.display = 'none';
             order.style.display = 'block';
             form_response.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
-
+    
         var product = document.getElementById("product-chat-5");
-        order.style.display = 'block'; 
         product.style.display = 'none';
     }
-}
+}    
 function showSecondReturnChat(chat) {
     var chat_button = document.getElementById(chat);
     disablePreviousChat(chat_button);
     user_chat_height = document.getElementById(chat).clientHeight; // Get the height of the user chat
 
-    if (chat === "return-chat-1") {
-        // Turn off other buttons of same category
+     if (chat === "return-chat-1") {
+        // Turn off other buttons of the same category
         var product = document.getElementById("return-chat-2");
         product.style.display = 'none';
         var return1 = document.getElementById("return-chat-3");
         return1.style.display = 'none';
         var shush = document.getElementById("return-chat-4");
         shush.style.display = 'none';
-
-        // Show AI response with a delay
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
         var order = document.getElementById("return-response-1");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        // Show AI response with a delay
         setTimeout(function() {
+            loader.style.display = 'none';
             order.style.marginBlockStart = user_chat_height + 30 + 'px';
             order.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
-
+    
         // Get next user option for return
         var return_chat_1 = document.getElementById("return-chat-5");
         var return_chat_2 = document.getElementById("return-chat-6");
-
+    
         // Add a delay and show the next user option
         setTimeout(function() {
             ai_chat_height = order.clientHeight;
@@ -463,8 +537,9 @@ function showSecondReturnChat(chat) {
             return_chat_1.style.display = 'block';
             return_chat_2.style.display = 'block';
         }, 2500);
-
-    } else if (chat === "return-chat-2") {
+    }
+    
+    else if (chat === "return-chat-2") {
         // Geth height of return-response-1
         var return_response_1 = document.getElementById("return-response-1");
         var return_response_1_height = return_response_1.clientHeight;
@@ -472,66 +547,100 @@ function showSecondReturnChat(chat) {
         var return_chat_2 = document.getElementById("return-chat-2");
         return_chat_2.style.marginTop = return_response_1_height + 30 + 'px';
         
-        // Turn off other buttons of same category
+        // Turn off other buttons of the same category
         var order = document.getElementById("return-chat-1");
         order.style.display = 'none';
         var return1 = document.getElementById("return-chat-3");
         return1.style.display = 'none';
         var shush = document.getElementById("return-chat-4");
         shush.style.display = 'none';
-
-        // Show AI response with a delay
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
         var product = document.getElementById("return-response-2");
+        product.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        // Show AI response with a delay
         setTimeout(function() {
+            loader.style.display = 'none';
             product.style.marginTop = user_chat_height + 30 + 'px';
             product.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
-
+    
         // Get next user option for return
         var return_chat_1 = document.getElementById("return-chat-5");
         var return_chat_2 = document.getElementById("return-chat-6");
-
+    
         // Add a delay and show the next user option
         setTimeout(function() {
             return_chat_1.style.display = 'block';
             return_chat_2.style.display = 'block';
             return_chat_1.style.marginTop = '120px';
         }, 2500);
-    } 
+    }
+    
     else if (chat === "return-chat-3") {
         var order = document.getElementById("return-response-3");
         var product = document.getElementById("return-response-3");
-        product.style.display = 'block';
+    
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        product.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        // Show AI response with a delay
+        setTimeout(function() {
+            loader.style.display = 'none';
+            product.style.display = 'block';
+        }, 2000); // 2000 milliseconds = 2 seconds
+    
         // Show next user option
         var return_chat_1 = document.getElementById("return-chat-5");
         var return_chat_2 = document.getElementById("return-chat-6");
+    
         setTimeout(function() {
             return_chat_1.style.display = 'block';
             return_chat_2.style.display = 'block';
             return_chat_1.style.marginTop = '120px';
         }, 2500);
+    
         var order = document.getElementById("return-chat-1");
         order.style.display = 'none';
-
+    
         var return1 = document.getElementById("return-chat-2");
         return1.style.display = 'none';
         var shush = document.getElementById("return-chat-4");
         shush.style.display = 'none';
     }
-    else{
+    
+    else {
         var shush = document.getElementById("return-response-4");
+        var form_response = document.getElementById("form-response");
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        shush.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
         setTimeout(function() {
+            loader.style.display = 'none';
             shush.style.display = 'block';
+            form_response.style.display = 'block';
         }, 2500);
+    
         var product = document.getElementById("return-chat-2");
         product.style.display = 'none';
-
+    
         var return1 = document.getElementById("return-chat-3");
         return1.style.display = 'none';
+    
         var order = document.getElementById("return-chat-1");
-        return1.style.display = 'none';
+        order.style.display = 'none';
     }
-}
+}    
 function showSecondReturnChat2(chat) {
     var chat_button = document.getElementById(chat);
     disablePreviousChat(chat_button);
@@ -540,37 +649,51 @@ function showSecondReturnChat2(chat) {
     if (chat === "return-chat-5") { // yes
         var order = document.getElementById("return-response-5");
         // use the chat variable to get the chat element
+        
+        // Show loader
+        var loader = document.getElementById("chat-loader");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
         setTimeout(function() {
+            loader.style.display = 'none';
             order.style.marginTop = user_chat_height + 30 + 'px';
             order.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
         
-        // Turn off other buttons of same category
+        // Turn off other buttons of the same category
         var product = document.getElementById("return-chat-6");
         product.style.display = 'none';
-    } else {  // no
-        // Add margin-top to no button.
-        var no_button = document.getElementById("return-chat-6");
-        no_button.style.marginTop = '180px';
-
-        // Turn off other buttons of same category
+    }
+    
+    else { // no
+        // Turn off other buttons of the same category
         var yes_button = document.getElementById("return-chat-5");
         yes_button.style.display = 'none';
-
+    
         var order = document.getElementById("return-response-6");
         var form_response = document.getElementById("form-response");
-
+    
+        // Show the loader
+        var loader = document.getElementById("chat-loader");
+        order.insertAdjacentElement('afterend', loader);
+        loader.style.marginTop = '10px';
+        loader.style.display = 'block';
+    
+        // Hide the form response
+        form_response.style.display = 'none';
+    
         // Show the AI response with a delay
         setTimeout(function() {
+            loader.style.display = 'none';
             order.style.marginTop = user_chat_height + 30 + 'px';
             order.style.display = 'block';
         }, 2000); // 2000 milliseconds = 2 seconds
-
-
+    
         // Show the form response with a delay
         setTimeout(function() {
             form_response.style.display = 'block';
-        }, 2100); // 2000 milliseconds = 2 seconds
-
+        }, 2100); // 2100 milliseconds = 2.1 seconds
     }
-}
+}   
