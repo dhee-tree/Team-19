51c1b120fa0b06aa22cf454d263f9ee6e4489182
Zaakir04 <220140434@aston.ac.uk>
2024-03-14 15:49:42 +0000

    Modified the controller to allow recent purchases from the database

--- WoodLess/app/Http/Controllers/UserPanelController.php
+++ WoodLess/app/Http/Controllers/UserPanelController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 use App\Models\Product;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class UserPanelController extends Controller
 {
     function show(){
         $user = auth()->user();
+        $recentOrder = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->first();
         return view('user-panel', [
             'user' => $user,
             'similarProducts'=> Product::all(),
+            'recent'=> $recentOrder,
         ]);
     }
 
