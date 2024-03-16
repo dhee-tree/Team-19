7c5bdc1f674f8037aa827133944d53808511b0df
Sgy157 <soggy706@googlemail.com>
2024-03-16 15:07:08 +0000

    Fixed typo that caused error

--- WoodLess/app/Http/Kernel.php
+++ WoodLess/app/Http/Kernel.php
@@ -66,7 +66,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'verify' => \App\Http\Middleware\verify::class,
+        'verify' => \App\Http\Middleware\Verify::class,
         'prevent.resubmission' => \App\Http\Middleware\PreventFormResubmission::class,
     ];
 }
