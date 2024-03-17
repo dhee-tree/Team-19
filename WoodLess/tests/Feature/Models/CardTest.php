e80f01d33b732528ad07fa01ca6ffcd2d22bf635
Sgy157 <soggy706@googlemail.com>
2024-03-17 15:46:03 +0000

    Added

---
+++ WoodLess/tests/Feature/Models/CardTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Card;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CardTest extends TestCase
+{
+    use RefreshDatabase;
+    protected Card $card;
+
+    /**
+     * Set up the model before each test.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->card = Card::factory()->create([
+            'user_id' => User::factory()->create(['first_name' => 'test'])->id,
+        ]);
+    }
+
+    /**
+     * Test to see if the model can its user.
+     */
+    public function test_review_model_can_get_user(): void
+    {   
+        $user = $this->card->user()->first();
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals('test', $user->first_name);
+    }
+}
